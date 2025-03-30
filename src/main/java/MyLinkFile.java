e896def002b848056896adea00f761201a8c4ed9
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-03-30 13:47:16 +0800

    draft

---
+++ src/main/java/MyLinkFile.java
@@ -0,0 +1,58 @@
+class MyLinkFileApp {
+    public static void main(String[] args) {
+        MyLinkList myLinkList = new MyLinkList();
+        myLinkList.addEl(new MyLinkNode(4));
+        myLinkList.addEl(new MyLinkNode(2));
+        myLinkList.addEl(new MyLinkNode(17));
+        myLinkList.traverse();
+    }
+}
+
+class MyLinkNode {
+    int val;
+    MyLinkNode next;
+
+    MyLinkNode(int val) {
+        this.val = val;
+    }
+}
+
+class MyLinkList {
+    MyLinkNode head;
+    int size;
+
+    MyLinkList() {
+        head = null;
+        size = 0;
+    }
+
+    MyLinkNode getLast() {
+        MyLinkNode last = head;
+        for (int i = 0; i < size && last != null && last.next != null; i++, last = last.next) {
+
+        }
+        return last;
+    }
+
+    void addEl(MyLinkNode el) {
+        MyLinkNode last = getLast();
+        if (last == null) {
+            head = el;
+        } else {
+            last.next = el;
+        }
+        size++;
+    }
+
+    void traverse() {
+        MyLinkNode current = head;
+        for (int i = 0; i < size && current != null; i++, current = current.next) {
+            System.out.print(current.val+",");
+        }
+        System.out.println();
+    }
+
+
+}
+
+
