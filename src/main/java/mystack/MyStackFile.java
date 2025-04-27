70096aaac13e16dad4de414b76cb9582eacbbb2a
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-04-28 02:41:06 +0800

    draft

--- src/main/java/mystack/MyStackFile.java
+++ src/main/java/mystack/MyStackFile.java
@@ -1,5 +1,10 @@
 package mystack;
 
+import common2.AryNode;
+import common2.DLinkNode;
+import common2.LinkNode;
+import common2.Node;
+
 class MyStackApp {
     public static void main(String[] args) {
         System.out.println("aaa");
@@ -8,12 +13,82 @@ class MyStackApp {
 
 
 
-interface MyStack {
-    void push();
+interface MyStack<T extends Node> {
+    void push(T el);
 
     void pop();
 
     void peek();
 
     void traverse();
+}
+
+class MyAryStack implements MyStack<AryNode> {
+
+
+    @Override
+    public void push(AryNode el) {
+
+    }
+
+    @Override
+    public void pop() {
+
+    }
+
+    @Override
+    public void peek() {
+
+    }
+
+    @Override
+    public void traverse() {
+
+    }
+}
+
+class MyLinkStack implements MyStack<LinkNode> {
+
+    @Override
+    public void push(LinkNode el) {
+
+    }
+
+    @Override
+    public void pop() {
+
+    }
+
+    @Override
+    public void peek() {
+
+    }
+
+    @Override
+    public void traverse() {
+
+    }
+}
+
+class MyDLinkStack implements MyStack<DLinkNode> {
+
+    @Override
+    public void push(DLinkNode el) {
+        
+    }
+
+    @Override
+    public void pop() {
+
+    }
+
+    @Override
+    public void peek() {
+
+    }
+
+    @Override
+    public void traverse() {
+
+    }
 }
