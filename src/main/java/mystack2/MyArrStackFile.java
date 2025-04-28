4fb224956cfc78506e92a51ace605ddc37407907
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-04-29 01:08:46 +0800

    draft

---
+++ src/main/java/mystack2/MyArrStackFile.java
@@ -0,0 +1,59 @@
+package mystack2;
+
+class MyArrStackApp {
+    public static void main(String[] args) {
+        AryStack aryStack = new AryStack();
+        aryStack.push(new Node(1));
+        aryStack.push(new Node(3));
+        aryStack.push(new Node(5));
+        aryStack.push(new Node(7));
+        aryStack.traverse();
+
+        Node pop = aryStack.pop();
+        Node pop1 = aryStack.pop();
+
+        Node top = aryStack.peek();
+
+        aryStack.traverse();
+    }
+}
+
+class Node {
+    int val;
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+class AryStack {
+    Node[] nodeAry;
+    int size;
+
+    AryStack() {
+        nodeAry = new Node[1000];
+        size = 0;
+    }
+
+    void push(Node node) {
+        nodeAry[size++] = node;
+    }
+
+    Node pop() {
+        Node ret = nodeAry[--size];
+        return  ret;
+    }
+
+    Node peek() {
+        Node ret = nodeAry[size-1];
+        return ret;
+    }
+
+    void traverse() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(nodeAry[i].val+",");
+        }
+        System.out.println();
+    }
+}
+
+
