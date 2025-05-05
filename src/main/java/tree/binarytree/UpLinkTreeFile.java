82d650908bca5074f6d8d2a84f51b032e95a110f
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-05-05 16:18:45 +0800

    draft

---
+++ src/main/java/tree/binarytree/UpLinkTreeFile.java
@@ -0,0 +1,20 @@
+package tree.binarytree;
+
+class UpLinkTreeApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
+
+class UpLinkNode {
+    int val;
+    UpLinkNode parent;
+
+    UpLinkNode(int val) {
+        this.val = val;
+    }
+}
+
+class UpLinkTree {
+    
+}
