79aa86a5cde2d63887e3f233dbe6cdefde90cdab
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-05-05 16:17:12 +0800

    draft

---
+++ src/main/java/tree/binarytree/UpAryTreeFile.java
@@ -0,0 +1,21 @@
+package tree.binarytree;
+
+class UpAryTreeApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
+
+class UpAryNode {
+    int val;
+    int parentIdx;
+    //depth? no can
+
+    UpAryNode(int val) {
+        this.val = val;
+    }
+}
+
+class UpAryTree {
+    //maxDepth? no can
+}
