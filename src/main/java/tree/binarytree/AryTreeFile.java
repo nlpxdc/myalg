f98248d8841a755123b435cba35849ee2d59ef5b
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-05-09 00:14:09 +0800

    draft

--- src/main/java/tree/binarytree/AryTreeFile.java
+++ src/main/java/tree/binarytree/AryTreeFile.java
@@ -2,7 +2,30 @@ package tree.binarytree;
 
 class AryTreeApp {
     public static void main(String[] args) {
-        System.out.println("aa");
+        Node nodea1 = new Node(1);
+
+        Node nodeb1 = new Node(2);
+        Node nodeb2 = new Node(3);
+
+        Node nodec1 = new Node(4);
+        Node nodec2 = new Node(5);
+        Node nodec3 = new Node(6);
+        Node nodec4 = new Node(7);
+
+        AryTree aryTree = new AryTree();
+        aryTree.size = 7;
+
+        aryTree.nodeAry[0] = nodea1;
+
+        aryTree.nodeAry[1] = nodeb1;
+        aryTree.nodeAry[2] = nodeb2;
+
+        aryTree.nodeAry[3] = nodec1;
+        aryTree.nodeAry[4] = nodec2;
+        aryTree.nodeAry[5] = nodec3;
+        aryTree.nodeAry[6] = nodec4;
+
+        aryTree.traverseBfs();
     }
 }
 
@@ -27,7 +50,10 @@ class AryTree {
     }
 
     void traverseBfs() {
-
+        for (int i = 0; i < size; i++) {
+            System.out.print(nodeAry[i].val+",");
+        }
+        System.out.println();
     }
 
 }
