271c698fe98f52714d6db1f6921f63e25fc2e59f
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-07-02 23:07:16 +0800

    draft

---
+++ src/main/java/sort/TimSorApp.java
@@ -0,0 +1,10 @@
+package sort;
+
+import java.util.Arrays;
+
+public class TimSorApp {
+    public static void main(String[] args) {
+        int[] ary = {3, 9, 5, 1};
+        Arrays.sort(ary);
+    }
+}
