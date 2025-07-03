c8258c19c9dbfe9a910fc36144fe3f4d10b212f1
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-07-03 23:05:44 +0800

    draft

--- src/main/java/sort/TimSortApp.java
+++ src/main/java/sort/TimSortApp.java
@@ -2,7 +2,7 @@ package sort;
 
 import java.util.Arrays;
 
-public class TimSorApp {
+public class TimSortApp {
     public static void main(String[] args) {
         int[] ary = {3, 9, 5, 1};
         Arrays.sort(ary);
