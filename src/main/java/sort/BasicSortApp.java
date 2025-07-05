4fb80764354a3746cffd254eb6796e31697865c5
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-07-05 12:54:17 +0800

    draft

--- src/main/java/sort/BasicSortApp.java
+++ src/main/java/sort/BasicSortApp.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class BasicSortApp {
     public static void main(String[] args) {
         //test sort methods
-        int[] ary = {8, 5, 3, 5, 8};
+        int[] ary = {8, 5, 3, 4, 5};
         insertionSort(ary);
         System.out.println(Arrays.toString(ary));
 
@@ -13,14 +13,16 @@ public class BasicSortApp {
 
 
     //1。插入排序 原地
+    //分两部分 有序无序，开始第一个默认有序部分，后面都是无序部分
     public static void insertionSort(int[] ary) {
+        //所以从无序部分第一个开始循环，要插入有序部分，所以要从index为1开始，0是有序部分
         for (int i = 1; i < ary.length; i++) {
-            int currentVal = ary[i];
+            int unsortedFirst = ary[i];
             int j = i-1;
-            for (; j >= 0 && ary[j] > currentVal ; j--) {
+            for (; j >= 0 && ary[j] > unsortedFirst ; j--) {
                 ary[j+1] = ary[j];
             }
-            ary[j+1] = currentVal;
+            ary[j+1] = unsortedFirst;
         }
     }
     //2。选择排序 原地
