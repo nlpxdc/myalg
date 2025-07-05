83d3791ac0d4538de97c580f1951aabc22d75844
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-07-05 21:36:43 +0800

    draft

--- src/main/java/sort/BasicSortApp.java
+++ src/main/java/sort/BasicSortApp.java
@@ -5,9 +5,12 @@ import java.util.Arrays;
 public class BasicSortApp {
     public static void main(String[] args) {
         //test sort methods
-        int[] ary = {8, 3, 5, 4, 5};
-        insertionSort(ary);
-        System.out.println(Arrays.toString(ary));
+//        int[] ary = {8, 3, 5, 4, 5};
+//        insertionSort(ary);
+//        System.out.println(Arrays.toString(ary));
+        int[] ary2 = {8, 3, 5, 4, 5};
+        selectionSort(ary2);
+        System.out.println(Arrays.toString(ary2));
 
     }
 
@@ -26,7 +29,19 @@ public class BasicSortApp {
         }
     }
     //2。选择排序 原地
-    public static void selectionSort(int[] ary) {}
+    public static void selectionSort(int[] ary) {
+        for (int i = 0; i < ary.length-1; i++) {
+            int currentMinIdx = i;
+            for (int j = i+1; j < ary.length && ary[currentMinIdx] > ary[j]; j++) {
+                currentMinIdx = j;
+            }
+            if (i != currentMinIdx) {
+                int t = ary[i];
+                ary[i] = ary[currentMinIdx];
+                ary[currentMinIdx] = t;
+            }
+        }
+    }
     //3。冒泡排序 原地
     public static void bubbleSort(int[] ary) {}
     //4。归并排序 非原地
