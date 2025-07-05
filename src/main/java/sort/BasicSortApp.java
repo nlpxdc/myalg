7a8b59911c484194eb068bab504a19b94c5f9cba
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-07-05 12:08:05 +0800

    draft

---
+++ src/main/java/sort/BasicSortApp.java
@@ -0,0 +1,20 @@
+package sort;
+
+public class BasicSortApp {
+    public static void main(String[] args) {
+
+    }
+
+
+    //1。插入排序 原地
+    public static void insertionSort(int[] ary) {}
+    //2。选择排序 原地
+    public static void selectionSort(int[] ary) {}
+    //3。冒泡排序 原地
+    public static void bubbleSort(int[] ary) {}
+    //4。归并排序 非原地
+    public static int[] mergeSort(int[] ary) {
+        return null;
+    }
+
+}
