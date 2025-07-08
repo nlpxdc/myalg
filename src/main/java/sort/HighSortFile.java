601a058d6edfa49f7220da83500ba1fb4820da0d
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-07-08 23:46:45 +0800

    draft

---
+++ src/main/java/sort/HighSortFile.java
@@ -0,0 +1,27 @@
+package sort;
+
+class HighSortApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+
+    //比较和交换，先比较，再交换，根据比较结果判断是否需要交换
+    //比较不动位置，只读，快
+    //交换动位置，写操作，慢
+
+    //1 冒泡排序+分区 快排
+    public static void quickSort(int[] ary) {
+
+    }
+    //记忆最深的算法，最鸡肋，不容易分类
+
+    //2 选择排序+堆 堆排序
+    public static void heapSort(int[] ary) {
+
+    }
+    //3 插入排序+分治 希尔排序 Shell排序 缩小增量排序 间隔序列 缩小间隔排序 增量序列，这里间隔就是增量，增量就是间隔
+    public static void reduceGapSort(int[] ary) {
+
+    }
+    
+}
