b8136de088bde48f981efa02d3e4b44de154d988
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-07-10 22:57:34 +0800

    draft

--- src/main/java/sort/AryBasicSortFile.java
+++ src/main/java/sort/AryBasicSortFile.java
@@ -3,6 +3,7 @@ package sort;
 //数组版本，后序可以再考虑链表版本
 //天然方便支持双引用（不可变指针）操作，指针，下标，索引，本质上就是地址操作
 //都是比较类(Compare)，交换类(Swap)，先比较 后交换，有必要再交换，更通用 O(nlogn)
+//todo 最后别忘了，从各个维度特性上去分析和整理每种算法
 class AryBasicSortApp {
     public static void main(String[] args) {
         //test sort methods
