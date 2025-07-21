858c90a67c0a7f34941e846230f1020831753076
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-07-21 22:00:52 +0800

    draft

--- src/main/java/sort/AryCntSortFile.java
+++ src/main/java/sort/AryCntSortFile.java
@@ -4,13 +4,16 @@ import java.util.Arrays;
 
 class AryCntSortApp {
     public static void main(String[] args) {
-        int[] ary = {5,7,2,5,1,6,5,8,6,2};
-        countingSort(ary);
-        System.out.println(Arrays.toString(ary));
+//        int[] ary = {5,7,2,5,1,6,5,8,6,2};
+//        countingSort(ary);
+//        System.out.println(Arrays.toString(ary));
+        int[] ary2 = {95,97,92,95,91,96,95,98,96,92};
+        countingSortV2(ary2);
+        System.out.println(Arrays.toString(ary2));
     }
 
 
-    //1 计数排序 counting sort
+    //1a 计数排序 counting sort
     static void countingSort(int[] ary) {
         int[] cntAry = new int[10];
 
@@ -24,6 +27,26 @@ class AryCntSortApp {
             int valCnt = cntAry[val];
             for (int i = 0; i < valCnt; i++) {
                 ary[globalIdx++] = val;
+            }
+        }
+    }
+
+    //1b 计数排序 counting sort
+    static void countingSortV2(int[] ary) {
+        int offset = 90;
+
+        int[] cntAry = new int[10];
+
+        for (int idx = 0; idx < ary.length; idx++) {
+            int val = ary[idx];
+            cntAry[val-offset]++;
+        }
+
+        int globalIdx = 0;
+        for (int val = 0; val < 9; val++) {
+            int valCnt = cntAry[val];
+            for (int i = 0; i < valCnt; i++) {
+                ary[globalIdx++] = val+offset;
             }
         }
     }
