924091ca96cce90e725a487769cde12929c1f8b3
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-01 23:16:48 +0800

    draft

--- src/main/java/sort/AryStringSortFile.java
+++ src/main/java/sort/AryStringSortFile.java
@@ -9,8 +9,8 @@ class AryStringSortApp {
     //空间复杂度O(C+N) 时间复杂度？
     static void radixSortLsd(int[] ary) {
         //1. 找到数组中的最大值，以确定最大位数
-        int max = max(ary);
-        int maxDigits = digits(max);
+        int max = MyUtil.max(ary);
+        int maxDigits = MyUtil.digits(max);
 
         //3. 创建一个临时数组，用于存储排序后的结果
         //辅助数组
@@ -47,35 +47,13 @@ class AryStringSortApp {
         }
     }
 
-    //3b 基数排序 radix sort MSD 字符串排序，不常用？场景：字符串很长、前缀重复极多、长度又不一致
+    //基数排序 radix sort MSD 字符串排序，场景：字符串、前缀重复多、长度又不一致
     //文件路径或类全限定名
-    //搜索引擎倒排词
-    //DNA 序列比对
 //    static void radixSortMsd(String[] strAry) {
 //
 //    }
 
-    // 假设输入是自然数
-    static int digits(int n) {
-        if (n == 0) {
-            return 1;
-        }
-        int cnt = 0;
-        for (int i = 0, d = n; i < Integer.MAX_VALUE && d>0; i++, d /= 10) {
-            cnt++;
-        }
-        return cnt;
-    }
 
-    static int max(int[] ary) {
-        int max = ary[0];
-        for (int i = 1; i < ary.length; i++) {
-            if (ary[i] > max) {
-                max = ary[i];
-            }
-        }
-        return max;
-    }
 
     //如何切分桶、如何组织数据、是否稳定、是否原地，有多种写法
     //2种，
