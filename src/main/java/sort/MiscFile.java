9ff7b0e0ddb1a050f5adaba0b43005adc9cc3331
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-02 00:20:36 +0800

    draft

--- src/main/java/sort/MiscFile.java
+++ src/main/java/sort/MiscFile.java
@@ -5,6 +5,9 @@ class MiscApp {
         System.out.println("aa");
     }
 
+    //基数排序，入参整型数组？ 不合适
+
+    //能做，但也不合适，稍好些
     //基数排序 排整型 按位处理 倒着来，最小位开始 radix sort LSD 整数排序
     //空间复杂度O(C+N)
     static void radixSortLsd(int[] ary) {
@@ -13,7 +16,7 @@ class MiscApp {
         int maxDigits = MyUtil.digits(max);
 
         //3. 创建一个临时数组，用于存储排序后的结果
-        //辅助数组
+        //辅助数组 O(n)的来源
         int[] tmpAry = new int[ary.length];
 
         for (int i = 1, exp = 1; i <= maxDigits; i++, exp *= 10) {
@@ -23,7 +26,7 @@ class MiscApp {
             //3. 计数
             for (int j = 0; j < ary.length; j++) {
                 int val = ary[j];
-                //余数
+                //余数 这里就在按位操作比较了 其实没必要
                 int remainder = (val / exp) % 10;
                 buckets[remainder]++;
             }
@@ -46,4 +49,11 @@ class MiscApp {
             System.arraycopy(tmpAry, 0, ary, 0, ary.length);
         }
     }
+
+//    //不合适 直接不用写了 因为按位处理，前面要补全，等价补全，补全以后等长按位处理会正确，否则就不对了
+//    //补全后，进制0-9可以排，当作一个整体比较，不然不能比，这样弄就不合适了，没必要
+//    static void radixSortMsd(int[] ary) {
+//
+//    }
+
 }
