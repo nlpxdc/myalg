36bae7ce4e088e73f792401d273a6f1feafe7ee9
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-02 00:46:46 +0800

    draft

--- src/main/java/sort/AryStringSortFile.java
+++ src/main/java/sort/AryStringSortFile.java
@@ -8,31 +8,32 @@ class AryStringSortApp {
 
     }
 
-
+    //如何切分桶、如何组织数据、是否稳定、是否原地，有多种写法
+    //主要有2种，一共有4种？
+    //American-flag sort 环交换 不稳定 原地 理论上 得不偿失 不常用 思路可借鉴
+    //链表桶排序（in-place list） 稳定 原地 理论上 得不偿失 不常用思路可借鉴
 
     //基数排序 radix sort MSD 字符串排序，场景：字符串、前缀重复多、长度又不一致
     //文件路径或类全限定名
-//    static void radixSortMsd(String[] strAry) {
-//
-//    }
 
-
-
-    //如何切分桶、如何组织数据、是否稳定、是否原地，有多种写法
-    //2种，
-    // 辅助数组+计数数组（递归计数版）；
-    // 3way radix quicksort
-
-    //自己的版本 非ai版本辅助
-    //基数排序 MSD 版本，适用于字符串数组，尤其是前缀类似的情况，以及不定长，可变长的字符串
-    //整数数组，可以简单认为是十进制数字的字符串表示 前面补全0，等长？
-    //只是字符取值范围只是纯数字，本身可以ascii码进行存储，这样可以方便比较lsd和msd
-    //todo 空间复杂度，时间复杂度
-    //空间复杂度O(C+N) 时间复杂度？
-    //如果是数字，十进制是个字符，桶
-    //如果是字符串，进制是256？扩展asc码128？asc码桶，即C的内容
-    static void radixSortMsd(int[] ary) {
+    //经典基础版 计数排序递归版 稳定 通用版 教学版 借鉴思路
+    //辅助数组+计数数组（递归计数版）
+    //先计数再递归？还是先递归再计数？方法名跟着改
+    static void radixSortMsdCntRecur(String[] strAry) {
 
     }
+
+    //高阶工程版 3way radix quicksort 不稳定 实际常用版
+    //三向切分快速排序（3-way radix quicksort）
+    //双或三指针原地交换
+    static void radixSortMsd3WayQuicksort(String[] strAry) {
+
+    }
+
+//    //这个其实不合适，按位比较字符串，从最后开始，不对齐啊，没必要，即使反转也不行，对齐的也不对
+//    //一般字符串比较不用这个，不直观不直觉，但是可能别的场景会合适，比如右对齐时排序？
+//    static void radixSortLsd(String[] strAry) {
+//
+//    }
 
 }
