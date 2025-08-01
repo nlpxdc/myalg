6cc1bce09a76369f0b3e84f8993f89160851235b
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-01 23:54:23 +0800

    draft

--- src/main/java/sort/AryCharSortFile.java
+++ src/main/java/sort/AryCharSortFile.java
@@ -2,58 +2,32 @@ package sort;
 
 //频次 统计 排序 也是一种分治
 //借助额外辅助数组aux 空间复杂度O(C)
+//假设范围为数字和英文字母
 class AryCharSortApp {
     public static void main(String[] args) {
-//        int[] ary = {5,7,2,5,1,6,5,8,6,2};
-//        countingSort(ary);
-//        System.out.println(Arrays.toString(ary));
-//        int digits = digits(2);
-
-//        int[] ary2 = {95,197,2692,595,7391,96,3895,98,996,92};
-//        int max = max(ary2);
-//        countingSortV2(ary2);
-        // radixSortLsd(ary2);
-        // System.out.println(Arrays.toString(ary2));
-//        String[] strAry = {"banana", "apple", "orange", "grape", "pear", "peach", "kiwi", "melon", "plum", "cherry"};
-//        radixSortMsd(strAry);
-//        System.out.println(Arrays.toString(strAry));
-    }
-
-
-    //1a 计数排序 counting sort
-    static void countingSort(int[] ary) {
-        int[] cntAry = new int[10];
-
-        for (int idx = 0; idx < ary.length; idx++) {
-            int val = ary[idx];
-            cntAry[val]++;
-        }
-
-        int globalIdx = 0;
-        for (int val = 0; val < 9; val++) {
-            int valCnt = cntAry[val];
-            for (int i = 0; i < valCnt; i++) {
-                ary[globalIdx++] = val;
-            }
-        }
+        char[] ary = {'d','f','k','e','v','j','e','p','j'};
+        countingSortV2(ary);
+        System.out.println(ary);
     }
 
     //1b 计数排序 counting sort 含偏移量
-    static void countingSortV2(int[] ary) {
-        int offset = 90;
+    static void countingSortV2(char[] ary) {
+        int offset = 'a';
 
-        int[] cntAry = new int[10];
+        //建桶，26个英文小写字母
+        int[] cntBuckets = new int[26];
 
         for (int idx = 0; idx < ary.length; idx++) {
-            int val = ary[idx];
-            cntAry[val-offset]++;
+            char c = ary[idx];
+            int cIdx = c;
+            cntBuckets[cIdx-offset]++;
         }
 
         int globalIdx = 0;
-        for (int val = 0; val < 9; val++) {
-            int valCnt = cntAry[val];
-            for (int i = 0; i < valCnt; i++) {
-                ary[globalIdx++] = val+offset;
+        for (char c = 'a'; c <= 'z'; c++) {
+            int cCnt = cntBuckets[c-offset];
+            for (int i = 0; i < cCnt; i++) {
+                ary[globalIdx++] = c;
             }
         }
     }
