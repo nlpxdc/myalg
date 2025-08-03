713d73c9f8c8b5909a5bbcd32c56c25e047cf4a2
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-03 12:52:18 +0800

    draft

--- src/main/java/search/MySearchFile.java
+++ src/main/java/search/MySearchFile.java
@@ -1,7 +1,12 @@
 package search;
 
+//查找第一个或者最后一个，查找多个的话就是字符串匹配，正则表达式，状态机了，有一整套完善的
+//普通场景，主键，值都不同，索引失效不！
 class MySearchApp {
     public static void main(String[] args) {
         System.out.println("aa");
+        String str = "abc";
+        char[] charArray = str.toCharArray();
+        int i = str.indexOf('b');
     }
 }
