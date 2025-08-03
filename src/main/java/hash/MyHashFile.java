d564be53e96ea1ca7c0e5261f8a286b731edaa47
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-03 10:15:10 +0800

    draft

---
+++ src/main/java/hash/MyHashFile.java
@@ -0,0 +1,7 @@
+package hash;
+
+class MyHashApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
---
+++ src/main/java/map/MyMapFile.java
@@ -0,0 +1,7 @@
+package map;
+
+class MyMapApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
