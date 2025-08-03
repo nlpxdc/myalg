f55a3628b7f92471b570a5e02721cba840abab72
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-03 14:32:02 +0800

    draft

--- src/main/java/alg/dynamicdesign/MyDynamicDesignFile.java
+++ src/main/java/alg/dynamicdesign/MyDynamicDesignFile.java
@@ -1,5 +1,6 @@
 package alg.dynamicdesign;
 
+//应用题，把现实抽象成数学模型，数据结构模型，再去考虑具体数据结构可以的操作，对应映射到实际问题上，然后求解操作，最后结果映射到实际问题上
 class MyDynamicDesignApp {
     public static void main(String[] args) {
         System.out.println("aa");
