e904e396e161f034f19cd66b80c68418916d472b
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-03 11:21:09 +0800

    draft

---
+++ src/main/java/alg/dynamicdesign/MyDynamicDesignFile.java
@@ -0,0 +1,7 @@
+package alg.dynamicdesign;
+
+class MyDynamicDesignApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
---
+++ src/main/java/alg/greedy/MyGreedyFile.java
@@ -0,0 +1,4 @@
+package alg.greedy;
+
+class MyGreedyApp {
+}
