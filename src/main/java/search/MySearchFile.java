a5ac98a2dfad9068fa083d2b2e2f67bf6fe24101
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-03 14:25:31 +0800

    draft

--- src/main/java/search/MySearchFile.java
+++ src/main/java/search/MySearchFile.java
@@ -14,10 +14,12 @@ class MySearchApp {
 //        int i1 = Arrays.binarySearch(charArray, 'b');
 //        int i2 = Collections.binarySearch(null, null);
         int[] ary = {0,1,2,3,5,8,13,21,34,55};
-        int i = idxOfIter(ary, 13);
-        int i2 = idxOfRecur(ary, 13);
-        int i1 = idxOfIter(ary, 14);
-        int i3 = idxOfRecur(ary, 14);
+//        int i = idxOfIter(ary, 13);
+//        int i2 = idxOfRecur(ary, 13);
+        int i4 = idxOfIterInterpolation(ary, 13);
+//        int i1 = idxOfIter(ary, 14);
+//        int i3 = idxOfRecur(ary, 14);
+//        int i5 = idxOfIterInterpolation(ary, 14);
 
 
     }
@@ -52,7 +54,7 @@ class MySearchApp {
 
     static int innerIdxOfRecur(int[] ary, int leftIdx, int rightIdx, int val) {
         if (leftIdx > rightIdx) {
-            return -1;
+            return -(leftIdx+1);
         }
         int midIdx = (leftIdx+rightIdx)/2;
         int midVal = ary[midIdx];
@@ -71,7 +73,45 @@ class MySearchApp {
     //为什么斐波那契？计算最简单 + 比例最好
 
     //3 插值思想
+    static int idxOfIterInterpolation(int[] ary, int val) {
+        int leftIdx = 0, rightIdx = ary.length-1;
 
+        for (int i = 0; i < Integer.MAX_VALUE && leftIdx<=rightIdx; i++) {
+            //midIdx 插值猜位置，不是简单的二分，要根据值来
+            int midIdx = calcMidIdx(ary, leftIdx, rightIdx, val);
 
+            //二分比较
+            int midVal = ary[midIdx];
+            if (val < midVal) {
+                rightIdx = midIdx-1;
+            } else if (midVal < val) {
+                leftIdx = midIdx+1;
+            } else {
+                return midIdx;
+            }
+        }
+
+        return -(leftIdx+1);
+    }
+
+    //测试起始是需要自己构造合适的测试数据，并来验证他优化方面的性能的
+    //但是测试数据造起来耗时，属于测试阶段，暂时先不管了，后续专门抽时间弄，先保证功能ok
+    //插值计算，根据value，查找对应所在合适的idx位置，更准的猜准接近的位置
+    static int calcMidIdx(int[] ary, int lowIdx, int highIdx, int val) {
+        int midIdx = lowIdx;
+        int lowVal = ary[lowIdx];
+        int highVal = ary[highIdx];
+        int valLowDelta = val - lowVal;
+        int idxWholeDelta = highIdx - lowIdx;
+        int valWholeDelta = highVal - lowVal;
+        if (valWholeDelta != 0) {
+            //线性等比例计算
+            int idxLowDelta = idxWholeDelta * valLowDelta / valWholeDelta;
+            midIdx = lowIdx + idxLowDelta;
+        }
+        //防止越界
+        midIdx = Math.max(lowIdx, Math.min(midIdx, highIdx));
+        return midIdx;
+    }
 
 }
