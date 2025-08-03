5fd1047c7bb3063c95e2f29a22015de9b10dec4e
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-03 12:57:26 +0800

    draft

--- src/main/java/search/MySearchFile.java
+++ src/main/java/search/MySearchFile.java
@@ -1,5 +1,8 @@
 package search;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 //查找第一个或者最后一个，查找多个的话就是字符串匹配，正则表达式，状态机了，有一整套完善的
 //普通场景，主键，值都不同，索引失效不！
 class MySearchApp {
@@ -8,5 +11,7 @@ class MySearchApp {
         String str = "abc";
         char[] charArray = str.toCharArray();
         int i = str.indexOf('b');
+        int i1 = Arrays.binarySearch(charArray, 'b');
+        int i2 = Collections.binarySearch(null, null);
     }
 }
