402aea4776d48ce8ffcf061320544dbb6dcd955c
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-10 19:29:12 +0800

    draft

---
+++ src/main/java/alg/CommonAlgApp.java
@@ -0,0 +1,10 @@
+package alg;
+
+public class CommonAlgApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
+
+//滑动窗口 sliding window 不变长度
+//滚动窗口 rolling window 可变长度
--- src/main/java/hash/MyHashFile.java
+++ src/main/java/hash/MyHashFile.java
@@ -3,5 +3,9 @@ package hash;
 class MyHashApp {
     public static void main(String[] args) {
         System.out.println("aa");
+
+        String str = "hello hash";
+        int i = str.hashCode();
     }
+
 }
