28715f570efa4c39b6600fe836d3c93430ffd553
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-13 00:49:27 +0800

    draft

--- src/main/java/hash/MyHashFile.java
+++ src/main/java/hash/MyHashFile.java
@@ -8,10 +8,55 @@ class MyHashApp {
         int i = str.hashCode();
     }
 
-    //极简
+    //教学 计算下标，索引位置
+//    1 加法hash
+    static int addHash(byte[] data, int mod) {
+        int hash = 0;
+        for (int i = 0; i < data.length; i++) {
+            byte byteData = data[i];
+            hash = (hash + byteData) % mod;
+        }
+        return hash;
+    }
+//    2 乘法hash
+    static int multiHash(byte[] data, int mod) {
+        int hash = 0;
+        for (int i = 0; i < data.length; i++) {
+            byte byteData = data[i];
+            hash = (hash*31+byteData) % mod;
+        }
+        return hash;
+    }
+//    3 异或hash
+    static int xorHash(byte[] data) {
+        int hash = 0;
+        for (int i = 0; i < data.length; i++) {
+            byte byteData = data[i];
+            hash ^= byteData;
+            hash = (hash << 4) ^ (hash >>> 2);
+        }
+        hash &= 0xFFFF;
+        return hash;
+    }
+//    4 旋转hash&滚动hash
+    static int rollingHash(byte[] data, int mod) {
+        int hash = 0;
+        for (int i = 0; i < data.length; i++) {
+            byte byteData = data[i];
+            hash = (hash*257+byteData) % mod;
+        }
+        return hash;
+    }
+//    5 组合hash FNV-1a
+    static int fnv1aHash(byte[] data) {
+        return 0;
+    }
+    //hash冲突解决的算法？
+
     //校验 CRC
+
     //一致性 ketama一致性hash环
 
-    //hash冲突解决的算法？
+
 
 }
