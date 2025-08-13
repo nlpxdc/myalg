111903237437eb0909f0c4d2f87b9740f3c1ce95
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-14 00:18:46 +0800

    draft

--- src/main/java/hash/MyHashFile.java
+++ src/main/java/hash/MyHashFile.java
@@ -5,6 +5,17 @@ import java.nio.charset.StandardCharsets;
 class MyHashApp {
     public static void main(String[] args) {
         System.out.println("aa");
+        int num = 0xFF;
+        int num2 = 0xff;
+        int num3 = 0xFf;
+        int num4 = 0xfF;
+        int num5 = 0Xff;
+        int num6 = 0Xf;
+        int num7 = 077;
+        int num8 = 076;
+        int num9 = 0b1;
+        int num10 = 0B0;
+        int num11 = 0B10;
 
         String str = "hello hashab";
         int i1 = addHash(str.getBytes(StandardCharsets.UTF_8), 8);
@@ -16,7 +27,13 @@ class MyHashApp {
     }
 
     //教学 计算下标，索引位置
+    //返回其实可以是定长的byte[], 比如md5的32位或16位，相当于16进制数，用字符串表示，大数
+    //32位=32*4=128bit位=16字节，16位=8字节，int 4字节 2个字 long 8字节 4个字
+    //target的hash空间int不够大 long还行 md5再翻倍，但是还是不安全，所以还是要256bit，区块链？ sha2-256 sha2-512？
+    //sha3？
+
 //    1 加法hash
+    //就是用上所有数据，这个不能算
     static int addHash(byte[] data, int mod) {
         int hash = 0;
         for (int i = 0; i < data.length; i++) {
@@ -26,6 +43,7 @@ class MyHashApp {
         return hash;
     }
 //    2 乘法hash
+    //这个算
     static int multiHash(byte[] data, int mod) {
         int hash = 0;
         for (int i = 0; i < data.length; i++) {
