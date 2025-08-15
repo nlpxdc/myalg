fb02656fb45b64a56175fc6659a7354bd7e65781
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-16 01:07:43 +0800

    draft

--- src/main/java/graph/MyGraphFile.java
+++ src/main/java/graph/MyGraphFile.java
@@ -1,8 +1,19 @@
 package graph;
 
 //简单图
-class MyGraphApp {
+class MatrixGraphApp {
     public static void main(String[] args) {
         System.out.println("aa");
     }
+}
+
+class MatrixGraph {
+    int[][] matrix;
+    int vertexSize;
+
+    MatrixGraph(int vertexSize) {
+        this.vertexSize = vertexSize;
+        this.matrix = new int[vertexSize][vertexSize];
+    }
+
 }
