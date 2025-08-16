4b9c79a2bf9c5f0500c8f3267c640c855b9937e0
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-16 22:58:32 +0800

    draft

---
+++ src/main/java/graph/AdjaMapMapGraphFile.java
@@ -0,0 +1,19 @@
+package graph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//有向带权图
+class AdjaMapMapGraphApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
+
+class AdjaMapMapGraph {
+    Map<Integer, Map<Integer, Integer>> adjaMapMap;
+
+    AdjaMapMapGraph() {
+        this.adjaMapMap = new HashMap<>();
+    }
+}
