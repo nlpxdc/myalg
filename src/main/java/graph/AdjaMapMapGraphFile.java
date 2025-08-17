ad7a95ff7162bf650c628c9214e2d5e15f5bae80
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-17 11:43:21 +0800

    draft

--- src/main/java/graph/AdjaMapMapGraphFile.java
+++ src/main/java/graph/AdjaMapMapGraphFile.java
@@ -3,6 +3,7 @@ package graph;
 import java.util.HashMap;
 import java.util.Map;
 
+//第三全局视角 不是矩阵，如果是稀疏，可以转成稀疏矩阵，可以直接用（稀疏）矩阵的计算能力，解全局问题
 //有向带权图
 class AdjaMapMapGraphApp {
     public static void main(String[] args) {
@@ -19,6 +20,6 @@ class AdjaMapMapGraph {
 
     //遍历 traverse
     //BFS
-    //DFS 无前中后，就一种
-    //判断有环
+    //DFS 无中，有前后？默认约定前序 后序 逆后序？ 唯一等价？
+    //判断有环无环 dag 拓扑排序多个？
 }
