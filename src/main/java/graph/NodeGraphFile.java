2cb6e0b880fe81a595ff2cbb5f48f546d31829d3
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-17 12:17:37 +0800

    draft

--- src/main/java/graph/NodeGraphFile.java
+++ src/main/java/graph/NodeGraphFile.java
@@ -11,13 +11,15 @@ class NodeGraphApp {
 }
 
 //自递归定义 定义无向工程有向，因为递归自身？简单图 权重是边的权重，点没有权重
+//如果有向怎么表示？下面那种，固定边权都是1，就不能是自递归定义了，自递归定义就是无向的
 class UnWeightedNode {
     int key;
     //无向，但是定义接口儿子，有点有向意思，可以有指回去的指针，但是不代表有向，只是容易回溯的手段而已
     Set<UnWeightedNode> neighbourSet;
 }
 
-// 不是无向，是有向！ 简单图
+// 不是无向，是有向！ 简单图 非自递归定义，节点含边（这个边也是对应一个节点的带权表示，key依旧是关键，weight次要），不是节点含自身节点
+//weight都等于1，就是有向无权图
 class FromWeightedNode {
     int from;
     Set<ToWeightedEdge> toWeightedEdgeSet;
