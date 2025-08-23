e0419d4521ad2bfc4c2ead865a2012916efb8fdb
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-23 09:21:59 +0800

    draft

--- src/main/java/graph/AdjaMatrixGraphFile.java
+++ src/main/java/graph/AdjaMatrixGraphFile.java
@@ -16,10 +16,23 @@ import java.util.Queue;
 //是方阵 每行每列必定代表一个顶点
 class AdjaMatrixGraphApp {
     public static void main(String[] args) {
-        AdjaUnDirectedUnWeightedMatrixGraph graph = new AdjaUnDirectedUnWeightedMatrixGraph(3);
+        AdjaUnDirectedUnWeightedMatrixGraph graph = new AdjaUnDirectedUnWeightedMatrixGraph(9);
         graph.addEdge(0,1);
         graph.addEdge(0,2);
         graph.addEdge(1,2);
+
+        graph.addEdge(0,3);
+        graph.addEdge(0,4);
+        graph.addEdge(3,4);
+
+        graph.addEdge(1,5);
+        graph.addEdge(1,6);
+        graph.addEdge(5,6);
+
+        graph.addEdge(2,7);
+        graph.addEdge(2,8);
+        graph.addEdge(7,8);
+        
         graph.traverseBfs();
     }
 }
