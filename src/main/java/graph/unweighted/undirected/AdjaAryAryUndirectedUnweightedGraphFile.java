bc0ade88b694660d84ccb1fc06ec6ac222852363
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-08-28 22:57:29 +0800

    draft

--- src/main/java/graph/unweighted/undirected/AdjaAryAryUndirectedUnweightedGraphFile.java
+++ src/main/java/graph/unweighted/undirected/AdjaAryAryUndirectedUnweightedGraphFile.java
@@ -20,7 +20,25 @@ import java.util.*;
 //依赖hash散列，减小存储空间
 class AdjaAryAryUndirectedUnweightedGraphApp {
     public static void main(String[] args) {
-        System.out.println("aa");
+        AdjaMapSetUndirectedUnweightedGraph graph = new AdjaMapSetUndirectedUnweightedGraph(9);
+        graph.addEdge(0,1);
+        graph.addEdge(0,2);
+        graph.addEdge(1,2);
+
+        graph.addEdge(0,3);
+        graph.addEdge(0,4);
+        graph.addEdge(3,4);
+
+        graph.addEdge(1,5);
+        graph.addEdge(1,6);
+        graph.addEdge(5,6);
+
+        graph.addEdge(2,7);
+        graph.addEdge(2,8);
+        graph.addEdge(7,8);
+
+        graph.singleBfs(0);
+
     }
 }
 
--- src/main/java/graph/unweighted/undirected/AdjaMatrixUndirectedUnweightedGraphFile.java
+++ src/main/java/graph/unweighted/undirected/AdjaMatrixUndirectedUnweightedGraphFile.java
@@ -19,34 +19,35 @@ class AdjaMatrixUndirectedUnweightedGraphApp {
 
     public static void main(String[] args) {
 //        AdjaUnDirectedUnWeightedMatrixGraph graph = new AdjaUnDirectedUnWeightedMatrixGraph(9);
-        AdjaMatrixUndirectedUnWeightedGraph graph = new AdjaMatrixUndirectedUnWeightedGraph(3);
+        AdjaMatrixUndirectedUnWeightedGraph graph = new AdjaMatrixUndirectedUnWeightedGraph(9);
         graph.addEdge(0,1);
         graph.addEdge(0,2);
-//        graph.addEdge(1,2);
-        graph.addEdge(2,1);
+        graph.addEdge(1,2);
+//        graph.addEdge(2,1);
 
-//        graph.addEdge(0,3);
-//        graph.addEdge(0,4);
-//        graph.addEdge(3,4);
-//
-//        graph.addEdge(1,5);
-//        graph.addEdge(1,6);
-//        graph.addEdge(5,6);
-//
-//        graph.addEdge(2,7);
-//        graph.addEdge(2,8);
-//        graph.addEdge(7,8);
+        graph.addEdge(0,3);
+        graph.addEdge(0,4);
+        graph.addEdge(3,4);
+
+        graph.addEdge(1,5);
+        graph.addEdge(1,6);
+        graph.addEdge(5,6);
+
+        graph.addEdge(2,7);
+        graph.addEdge(2,8);
+        graph.addEdge(7,8);
 
         //byEdge
-        graph.traverseVertexByEdge();
+//        graph.traverseVertexByEdge();
 
         //bfs
-        graph.bfs();
+//        graph.bfs();
+        graph.singleBfs(0);
 
         //dfs
-        graph.preOrderDfs();
-        graph.postOrderDfs();
-        graph.dfs();
+//        graph.preOrderDfs();
+//        graph.postOrderDfs();
+//        graph.dfs();
     }
 
 }
