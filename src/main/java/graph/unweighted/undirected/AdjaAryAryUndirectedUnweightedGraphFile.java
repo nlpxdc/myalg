f13b9ffd3a3d7dea8e3ad77d8c7447cac55fe468
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-09-04 22:53:19 +0800

    draft

--- src/main/java/graph/unweighted/undirected/AdjaAryAryUndirectedUnweightedGraphFile.java
+++ src/main/java/graph/unweighted/undirected/AdjaAryAryUndirectedUnweightedGraphFile.java
@@ -94,33 +94,30 @@ class AdjaMapSetUndirectedUnweightedGraph {
         Queue<Integer> queue = new LinkedList<>();
 
         //这里可以是任意startV n
-        visited[startV] = true;
         queue.offer(startV);
         while (!queue.isEmpty()) {
             //先访问当前自己
             Integer v = queue.poll();
-            //和下面的没有区别
+            visited[v] = true;
             GraphUtil.visit(v);
             //再按层访问邻接顶点 这里没有递归，所以访问写在前后无所谓，最终都是在前
             //这里就按照顺序从小到大，从左到右即可，反过来也行，但没什么本质区别
             Set<Integer> adjaUSet = adjaMapSet.getOrDefault(v, new HashSet<>());
             for (Integer adjaU : adjaUSet) {
                 if (!visited[adjaU]) {
-                    visited[adjaU] = true;
                     queue.offer(adjaU);
                 }
             }
-//            visit(v);
         }
     }
     //dfs
     void dfs(int v, boolean[] visited) {
+        visited[v] = true;
         //前序遍历
         GraphUtil.discover(v);
         Set<Integer> adjaUSet = adjaMapSet.getOrDefault(v, new HashSet<>());
         for (Integer adjaU : adjaUSet) {
             if (!visited[adjaU]) {
-                visited[adjaU] = true;
                 dfs(adjaU, visited);
             }
         }
