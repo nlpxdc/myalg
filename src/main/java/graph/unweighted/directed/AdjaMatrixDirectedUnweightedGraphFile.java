34eda6e8c1fd5f785190deedd05e0609572dbbc2
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-09-04 22:36:11 +0800

    draft

--- src/main/java/graph/unweighted/directed/AdjaMatrixDirectedUnweightedGraphFile.java
+++ src/main/java/graph/unweighted/directed/AdjaMatrixDirectedUnweightedGraphFile.java
@@ -1,9 +1,5 @@
 package graph.unweighted.directed;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
 //第三他人全局视角 是矩阵 如果是稠密，直接用，不用转稀疏矩阵，直接矩阵计算，解全局问题
 //二维数组的表示，表示图
 //简单图 多重图？ 二维ary 二维vector 冗余视图结构
@@ -37,16 +33,11 @@ class AdjaMatrixDirectedUnweightedGraphApp {
         graph.addArc(2,8);
         graph.addArc(7,8);
 
-//        //byEdge
-//        graph.traverseVertexByEdge();
-
-        //bfs
-        graph.bfs();
-
-        //dfs
-        graph.preOrderDfs();
-        graph.postOrderDfs();
-        graph.dfs();
+//        //bfs
+//        graph.bfs();
+//
+//        //dfs
+//        graph.dfs();
     }
 
 }
@@ -78,235 +69,17 @@ class AdjaMatrixDirectedUnweightedGraph {
         }
     }
 
-    Integer getFirstUnVisited(boolean[] visited) {
-        for (int i = 0; i < visited.length; i++) {
-            if (!visited[i]) {
-                return i;
-            }
-        }
-        return null;
-    }
-    void visit(int v) {
-        System.out.print(v+",");
-    }
-    void discover(int v) {
-        System.out.println(String.format("discover %d", v));
-    }
-    void finish(int v) {
-        System.out.println(String.format("finish %d", v));
-    }
 
-    boolean beNullGraph() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (adjaMatrix[i][j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    boolean beAllVisited(boolean[] visited) {
-        for (int i = 0; i < visited.length; i++) {
-            if (!visited[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-    void resetVisited(boolean[] visited) {
-        for (int i = 0; i < n; i++) {
-            visited[i] = false;
-        }
-        Arrays.fill(visited, false);
-    }
 
     //计算连通子图的个数，连通分量，任意一个订单开始遍历，然后标记访问列表，然后再取一个顶点，从没被标记过的顶点中选，再标记，直到所有节点被标记过为止
     //那遍历了多少次，就是有多少个连通子图，也就是连通分量，用是否访问标记进行判断，被访问过和是否连通是两个事情
     //多扫一遍顶点、数连通分量
     //判断连通子图个数，连通分量
 
-//    //遍历，实质就是第三他人视角 2维数组的循环，2层外里嵌套循环
-//    //注意这里访问的是边，然后要转成点 这个逻辑上没啥作用
-//    void traverseVertexByEdge() {
-//        boolean[] visited = new boolean[n];
-//
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < n; j++) {
-//                if (adjaMatrix[i][j]) {
-//                    if (!visited[i]) {
-//                        visited[i] = true;
-//                        visit(i);
-//                    }
-//                    if (!visited[j]) {
-//                        visited[j] = true;
-//                        visit(j);
-//                    }
-//                }
-//            }
-//        }
-//
-//        System.out.println();
-//        System.out.println();
-//    }
-
     //bfs
-    //假设有且只有一个连通子图 访问这个顶点的连通子图的所有顶点
-    void singleBfs(int startV) {
-        boolean[] visited = new boolean[n];
 
-        innerBfs(startV, visited);
+    //dfs
 
-        System.out.println();
-    }
-    int bfs() {
-        //初始化临时数组，记录访问状态
-        boolean[] visited = new boolean[n];
-
-        int childrenGraphCount = 0;
-        for (int i = 0; i < n; i++) {
-            Integer firstUnVisited = getFirstUnVisited(visited);
-            if (firstUnVisited != null) {
-                childrenGraphCount++;
-                innerBfs(firstUnVisited, visited);
-            } else {
-                break;
-            }
-        }
-
-        System.out.println();
-
-        return childrenGraphCount;
-    }
-    void innerBfs(int startV, boolean[] visited) {
-        //临时队列
-        Queue<Integer> queue = new LinkedList<>();
-
-        //这里可以是任意startV n
-        queue.offer(startV);
-        visited[startV] = true;
-        while (!queue.isEmpty()) {
-            Integer currentV = queue.poll();
-            visit(currentV);
-            for (int adjaV = 0; adjaV < n; adjaV++) {
-                if (adjaMatrix[currentV][adjaV] && !visited[adjaV]) {
-                    visited[adjaV] = true;
-                    queue.offer(adjaV);
-                }
-            }
-        }
-
-        System.out.println();
-    }
-
-    //dfs preOrder & postOrder
-    void singleDfs(int startV) {
-        boolean[] visited = new boolean[n];
-
-        innerDfs(startV, visited);
-
-        System.out.println();
-    }
-    int dfs() {
-        boolean[] visited = new boolean[n];
-
-        int childrenGraphCount = 0;
-        for (int i = 0; i < n; i++) {
-            Integer firstUnVisited = getFirstUnVisited(visited);
-            if (firstUnVisited != null) {
-                innerDfs(firstUnVisited, visited);
-                System.out.println();
-            } else {
-                break;
-            }
-        }
-
-        System.out.println();
-
-        return childrenGraphCount;
-    }
-    void innerDfs(int v, boolean[] visited) {
-        visited[v] = true;
-        discover(v);
-        for (int u = 0; u < n; u++) {
-            if (adjaMatrix[v][u] && !visited[u]) {
-                innerDfs(u, visited);
-            }
-        }
-        finish(v);
-    }
-
-    //dfs preOrder
-    void singlePreOrderDfs(int startV) {
-        boolean[] visited = new boolean[n];
-
-        innerPreOrderDfs(startV, visited);
-
-        System.out.println();
-    }
-    int preOrderDfs() {
-        boolean[] visited = new boolean[n];
-
-        int childrenGraphCount = 0;
-        for (int i = 0; i < n; i++) {
-            Integer firstUnVisited = getFirstUnVisited(visited);
-            if (firstUnVisited != null) {
-                innerPreOrderDfs(firstUnVisited, visited);
-                System.out.println();
-            } else {
-                break;
-            }
-        }
-
-        System.out.println();
-
-        return childrenGraphCount;
-    }
-    void innerPreOrderDfs(int v, boolean[] visited) {
-        visited[v] = true;
-        visit(v);
-        for (int u = 0; u < n; u++) {
-            if (adjaMatrix[v][u] && !visited[u]) {
-                innerPreOrderDfs(u, visited);
-            }
-        }
-    }
-
-    //dfs postOrder
-    void singlePostOrderDfs(int startV) {
-        boolean[] visited = new boolean[n];
-
-        innerPostOrderDfs(startV, visited);
-
-        System.out.println();
-    }
-    int postOrderDfs() {
-        boolean[] visited = new boolean[n];
-
-        int childrenGraphCount = 0;
-        for (int i = 0; i < n; i++) {
-            Integer firstUnVisited = getFirstUnVisited(visited);
-            if (firstUnVisited != null) {
-                innerPostOrderDfs(firstUnVisited, visited);
-                System.out.println();
-            } else {
-                break;
-            }
-        }
-
-        System.out.println();
-
-        return childrenGraphCount;
-    }
-    void innerPostOrderDfs(int v, boolean[] visited) {
-        visited[v] = true;
-        for (int u = 0; u < n; u++) {
-            if (adjaMatrix[v][u] && !visited[u]) {
-                innerPostOrderDfs(u, visited);
-            }
-        }
-        visit(v);
-    }
 }
 
 ////边遍历就算了不关心
