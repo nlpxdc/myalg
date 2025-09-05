ed2bf895c8c93e282139a5edee07119613eb2d3b
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-09-05 09:10:26 +0800

    draft

--- src/main/java/graph/unweighted/directed/AdjaMapSetDirectedUnweightedGraphFile.java
+++ src/main/java/graph/unweighted/directed/AdjaMapSetDirectedUnweightedGraphFile.java
@@ -96,7 +96,6 @@ class AdjaMapSetDirectedUnweightedGraph {
         while (!queue.isEmpty()) {
             //先访问当前自己
             Integer v = queue.poll();
-            visited[v] = true;
             GraphUtil.visit(v);
             //再按层访问邻接顶点 这里没有递归，所以访问写在前后无所谓，最终都是在前
             //这里就按照顺序从小到大，从左到右即可，反过来也行，但没什么本质区别
