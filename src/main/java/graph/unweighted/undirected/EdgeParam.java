d6ad855050d965c40ba58da0e98a916a5f1c4767
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-09-26 10:57:32 +0800

    draft

---
+++ src/main/java/graph/unweighted/undirected/EdgeParam.java
@@ -0,0 +1,27 @@
+package graph.unweighted.undirected;
+
+public class EdgeParam {
+    public Integer smallV;
+    public Integer bigV;
+
+    public EdgeParam(Integer v, Integer u) {
+        if (v == null || u == null) {
+            throw new RuntimeException("v or u null");
+        }
+        if (v < u) {
+            this.smallV = v;
+            this.bigV = u;
+        } else if (v > u){
+            this.smallV = u;
+            this.bigV = v;
+        } else {
+            throw new RuntimeException("v cannot equals u");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Edge:%d->%d", smallV, bigV);
+    }
+    
+}
