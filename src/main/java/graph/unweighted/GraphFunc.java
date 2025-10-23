d425541c516801a61660bff16be47fc2089c4f66
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-10-24 02:50:28 +0800

    draft

--- src/main/java/graph/unweighted/GraphFunc.java
+++ src/main/java/graph/unweighted/GraphFunc.java
@@ -1,9 +1,9 @@
 package graph.unweighted;
 
 @FunctionalInterface
-public interface GraphFunc<SingleStartParam, GraphTempVo, SingleVo, AllVo> {
+public interface GraphFunc<SingleStartParam, AllTemp, SingleVo, AllVo> {
 
 //    SingleVo accept(SingleStartParam singleStartParam, GraphTempVo graphTempVo);
-    void singleTraverse(SingleStartParam singleStartParam, GraphTempVo graphTempVo, SingleVo singleVo, AllVo allVo);
+    void singleTraverse(SingleStartParam singleStartParam, AllTemp allTemp, SingleVo singleVo, AllVo allVo);
 
 }
