29810bf5172188d6e67c5fad3358691c11eb89af
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-02 18:53:19 +0800

    draft

---
+++ src/main/java/alg2/o1/Q001Sum100File.java
@@ -0,0 +1,13 @@
+package alg2.o1;
+
+class Q001Sum100File {
+    public static void main(String[] args) {
+        int fn = fn(100);
+        int fn1 = fn(101);
+    }
+
+    static int fn(int n) {
+        return  (1+n)*n/2;
+    }
+
+}
