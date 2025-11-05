4a95d88628f074ddc4fe6e83b7c03b29b4586d60
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-05 23:15:18 +0800

    draft

--- src/main/java/alg/bitwise/MyBitwiseFile.java
+++ src/main/java/alg/bitwise/MyBitwiseFile.java
@@ -1,7 +1,15 @@
 package alg.bitwise;
 
+import java.math.BigInteger;
+
 class MyBitwiseApp {
     public static void main(String[] args) {
         System.out.println("aa");
+
+        BigInteger base = new BigInteger("7");
+        BigInteger exp  = new BigInteger("2025");
+        BigInteger mod  = new BigInteger("1000000007");
+        System.out.println(base.modPow(exp, mod)); // 7^2025 % 1e9+7
+
     }
 }
