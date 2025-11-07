d7bfe3c2f254891e418b0f5bad9da69f38035e02
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-08 00:31:19 +0800

    draft

--- src/main/java/storage/linked/MyLinkedFile.java
+++ src/main/java/storage/linked/MyLinkedFile.java
@@ -3,5 +3,18 @@ package storage.linked;
 class MyLinkedFile {
     public static void main(String[] args) {
         System.out.println("aa");
+
+        MyNode myNode = new MyNode();
+        MyNode myNode1 = new MyNode();
+        MyNode myNode2 = new MyNode();
+        myNode.next = myNode1;
+        myNode1.next = myNode2;
+        myNode2.next = null;
+
     }
+}
+
+class MyNode {
+    Object val;
+    MyNode next;
 }
