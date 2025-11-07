f46c9f57ab6d8636c027e8be266e27eb913183f6
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-07 08:29:12 +0800

    draft

---
+++ src/main/java/alg/bitwise/BitMaskFile.java
@@ -0,0 +1,7 @@
+package alg.bitwise;
+
+class BitMaskFile {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
---
+++ src/main/java/storage/simlinkedbycontiguous/StaticLinkedListFile.java
@@ -0,0 +1,7 @@
+package storage.simlinkedbycontiguous;
+
+class StaticLinkedListFileApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
