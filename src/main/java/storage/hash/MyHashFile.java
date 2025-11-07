5742924c7638487e11c436ae3fc53cac82942ef5
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-08 01:56:50 +0800

    draft

--- src/main/java/storage/hash/MyHashFile.java
+++ src/main/java/storage/hash/MyHashFile.java
@@ -4,27 +4,47 @@ class MyHashFile {
     public static void main(String[] args) {
         System.out.println("aa");
 
-        Object[] objects = new Object[100000];
+//        Object[] objects = new Object[100000];
+//
+//        Object o0 = new Object();
+//        int i0 = o0.hashCode() % 100000;
+//
+//        Object o1 = new Object();
+//        int i1 = o1.hashCode() % 100000;
+//
+//        Object o2 = new Object();
+//        int i2 = o2.hashCode() % 100000;
+//
+//        Object o3 = new Object();
+//        int i3 = o3.hashCode() % 100000;
+//
+////        myHashCode()
+//        objects[i0] = o0;
+//        objects[i1] = o1;
+//        objects[i2] = o2;
+//        objects[i3] = o3;
 
-        Object o0 = new Object();
-        int i0 = o0.hashCode() % 100000;
+        String[] stringAry = new String[100000];
 
-        Object o1 = new Object();
-        int i1 = o1.hashCode() % 100000;
+        String abc = new String("abc");
+        String def = new String("def");
+        String ghi = new String("ghi");
 
-        Object o2 = new Object();
-        int i2 = o2.hashCode() % 100000;
+        stringAry[myHashCode("abc")] = abc;
+        stringAry[myHashCode("def")] = def;
+        stringAry[myHashCode("ghi")] = ghi;
 
-        Object o3 = new Object();
-        int i3 = o3.hashCode() % 100000;
+    }
 
-//        myHashCode()
-        objects[i0] = o0;
-        objects[i1] = o1;
-        objects[i2] = o2;
-        objects[i3] = o3;
-
-
+    static int myHashCode(String str) {
+        int h = 0;
+        char[] charArray = str.toCharArray();
+        if (charArray.length > 0) {
+            for (int i = 0; i < charArray.length; i++) {
+                h = 31 * h + charArray[i];
+            }
+        }
+        return h;
     }
 
 //    static int myHashCode(int[] val) {
