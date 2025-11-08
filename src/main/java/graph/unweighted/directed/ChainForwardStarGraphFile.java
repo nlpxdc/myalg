137abed2a031b3a65016dec9785bb1126c40b6ba
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-08 20:38:30 +0800

    draft

--- src/main/java/graph/unweighted/directed/ChainForwardStarGraphFile.java
+++ src/main/java/graph/unweighted/directed/ChainForwardStarGraphFile.java
@@ -1,6 +1,11 @@
 package graph.unweighted.directed;
 
+import graph.unweighted.AllVo;
+import graph.unweighted.GraphMeta;
+
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 class ChainForwardStarGraphApp {
     public static void main(String[] args) {
@@ -14,8 +19,7 @@ class ChainForwardStarGraphApp {
 
 }
 
-class ChainForwardStarGraph {
-    int n;
+class ChainForwardStarGraph extends GraphMeta {
     int outArcCnt;
     int[] vertexOutArcsHeadIdxAry;
     OutArc[] outArcAry;
@@ -33,6 +37,31 @@ class ChainForwardStarGraph {
         OutArc outArc = new OutArc(to, origFromHeadIdx);
         outArcAry[outArcCnt-1] = outArc;
         vertexOutArcsHeadIdxAry[from] = outArcCnt-1;
+    }
+
+    @Override
+    public AllVo allTraverseByBfs() {
+        return null;
+    }
+
+    @Override
+    public AllVo allTraverseByDfs() {
+        return null;
+    }
+
+    @Override
+    public Map<Integer, Integer> calcInDegreeMap() {
+        return null;
+    }
+
+    @Override
+    public List<Integer> allTopoOrderByBfs() {
+        return null;
+    }
+
+    @Override
+    public List<Integer> allTopoOrderByDfs() {
+        return null;
     }
 
     //写下bfs和dfs，应该可以继承元图抽象类
