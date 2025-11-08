e622250564daa0ee993785b1d2830e8100ad794f
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-08 20:35:30 +0800

    draft

---
+++ src/main/java/graph/unweighted/directed/ChainForwardStarGraphFile.java
@@ -0,0 +1,40 @@
+package graph.unweighted.directed;
+
+import java.util.Arrays;
+
+class ChainForwardStarGraphApp {
+    public static void main(String[] args) {
+        ChainForwardStarGraph chainForwardStarGraph = new ChainForwardStarGraph(3);
+
+        chainForwardStarGraph.addArc(0,1);
+        chainForwardStarGraph.addArc(0,2);
+        chainForwardStarGraph.addArc(1,2);
+        chainForwardStarGraph.addArc(2,1);
+    }
+
+}
+
+class ChainForwardStarGraph {
+    int n;
+    int outArcCnt;
+    int[] vertexOutArcsHeadIdxAry;
+    OutArc[] outArcAry;
+
+    public ChainForwardStarGraph(int n) {
+        this.n = n;
+        this.vertexOutArcsHeadIdxAry = new int[n];
+        Arrays.fill(this.vertexOutArcsHeadIdxAry, -1);
+        this.outArcAry = new OutArc[100000];
+    }
+
+    void addArc(int from, int to) {
+        outArcCnt++;
+        int origFromHeadIdx = vertexOutArcsHeadIdxAry[from];
+        OutArc outArc = new OutArc(to, origFromHeadIdx);
+        outArcAry[outArcCnt-1] = outArc;
+        vertexOutArcsHeadIdxAry[from] = outArcCnt-1;
+    }
+
+    //写下bfs和dfs，应该可以继承元图抽象类
+
+}
---
+++ src/main/java/graph/unweighted/directed/OutArc.java
@@ -0,0 +1,11 @@
+package graph.unweighted.directed;
+
+public class OutArc {
+    int to;
+    int nextIdx;
+
+    public OutArc(int to, int nextIdx) {
+        this.to = to;
+        this.nextIdx = nextIdx;
+    }
+}
