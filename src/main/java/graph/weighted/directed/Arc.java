4d1fd11fcde0837ff1ca2dcac4709e1247b60a83
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-08 12:31:04 +0800

    draft

---
+++ src/main/java/graph/weighted/directed/Arc.java
@@ -0,0 +1,14 @@
+package graph.weighted.directed;
+
+public class Arc {
+    int from;
+    int to;
+    int weight;
+
+    public Arc(int from , int to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+}
