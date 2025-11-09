07abe51f823c5f9e2f417731c61558ef540200f1
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-09 12:12:48 +0800

    draft

--- src/main/java/graph/unweighted/directed/AdjaMapSetDirectedUnweightedGraphFileStaticVer.java
+++ src/main/java/graph/unweighted/directed/AdjaMapSetDirectedUnweightedGraphFileStaticVer.java
@@ -1,11 +1,8 @@
 package graph.unweighted.directed;
 
-import graph.unweighted.AllVo;
-import graph.unweighted.GraphMeta;
+import graph.unweighted.*;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class ChainForwardStarGraphApp {
     public static void main(String[] args) {
@@ -41,7 +38,9 @@ class ChainForwardStarGraph extends GraphMeta {
 
     @Override
     public AllVo allTraverseByBfs() {
-        return null;
+        AllVo allVo = new AllVo();
+        GraphUtil.allTraverse(this, this::singleTraverseByBfs, allVo);
+        return allVo;
     }
 
     @Override
@@ -65,5 +64,56 @@ class ChainForwardStarGraph extends GraphMeta {
     }
 
     //写下bfs和dfs，应该可以继承元图抽象类
+    //bfs
+    void singleTraverseByBfs(final VParam singleStartVParam,
+                             final SingleTemp singleTemp,
+                             final AllTemp allTemp,
+                             final SingleVo singleVo,
+                             final AllVo allVo) {
+        singleVo.directed = true;
+        //临时队列
+        Queue<VParam> queue = new LinkedList<>();
+
+        //这里可以是任意startV n
+        allTemp.visited[singleStartVParam.v] = true;
+        allTemp.parents[singleStartVParam.v] = null;
+        singleStartVParam.bfsVLevel = 0;
+        queue.offer(singleStartVParam);
+
+        while (!queue.isEmpty()) {
+            //先访问当前自己
+            VParam vParam = queue.poll();
+            GraphUtil.bfsVisitV(vParam, singleTemp, allTemp, singleVo, allVo);
+            //再按层访问邻接顶点 这里没有递归，所以访问写在前后无所谓，最终都是在前
+            //这里就按照顺序从小到大，从左到右即可，反过来也行，但没什么本质区别
+
+            //根据顶点v获取v的所有邻接节点(出顶点)（通过邻接出边弧arc）
+//            Set<Integer> adjaUSet = adjaMapSet.get(vParam.v);
+            int vOutArcsHeadIdx = vertexOutArcsHeadIdxAry[vParam.v];
+            OutArc currOutArc = outArcAry[vOutArcsHeadIdx];
+            for (int i = 0;
+                 i < outArcCnt && currOutArc != null;
+                 i++, currOutArc = currOutArc.nextIdx < 0 ? null : outArcAry[currOutArc.nextIdx]) {
+
+                Integer adjaU = currOutArc.to;
+                ArcParam arcParam = new ArcParam(vParam.v, adjaU);
+
+                if (!allTemp.visited[adjaU]) {
+                    arcParam.bfsArcType = BfsArcType.BFS_TREE_ARC;
+                    GraphUtil.bfsVisitArc(arcParam, singleVo);
+                    allTemp.visited[adjaU] = true;
+                    VParam uParam = new VParam(vParam.v, adjaU);
+                    allTemp.parents[adjaU] = vParam.v;
+                    uParam.bfsVLevel = vParam.bfsVLevel+1;
+                    queue.offer(uParam);
+                } else {
+                    arcParam.bfsArcType = BfsArcType.BFS_NON_TREE_ARC;
+                    GraphUtil.bfsVisitArc(arcParam, singleVo);
+                }
+
+            }
+
+        }
+    }
 
 }
