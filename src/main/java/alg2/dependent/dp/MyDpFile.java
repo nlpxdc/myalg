b608f245d0c708963706095ef1ee3464bebe77d6
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-28 00:27:29 +0800

    draft

--- src/main/java/alg2/dependent/dp/MyDpFile.java
+++ src/main/java/alg2/dependent/dp/MyDpFile.java
@@ -1,5 +1,5 @@
 package alg2.dependent.dp;
 
-//最优 系统设计 少见 离线 非实时t+1
+//最优 系统设计 近似 少见 离线 非实时t+1
 public class MyDpFile {
 }
--- src/main/java/alg2/dependent/greedy/MyGreedyFile.java
+++ src/main/java/alg2/dependent/greedy/MyGreedyFile.java
@@ -1,6 +1,6 @@
 package alg2.dependent.greedy;
 
-//最优 系统设计 常见 在线 准实时？t+0 t+e 满足贪心条件 贪心结构
+//最优 系统设计 精准 常见 在线 准实时？t+0 t+e 满足贪心条件 贪心结构
 public class MyGreedyFile {
     public static void main(String[] args) {
         System.out.println("aa");
