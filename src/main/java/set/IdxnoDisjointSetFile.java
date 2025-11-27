0c8f2f27474f90cfa6efe4b9966932bf6b0e681e
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-28 02:11:31 +0800

    draft

--- src/main/java/set/IdxnoDisjointSetFile.java
+++ src/main/java/set/IdxnoDisjointSetFile.java
@@ -1,5 +1,10 @@
 package set;
 
+//是集合，单纯集合，只有元素没有边的图 空图？连通分量没有or最大？
+//也是图，无向无权图，只讲连通关系，等价关系，一类关系，根据此优化存储结构，映射，切面
+//也是森林，树，连通无环，多个连通分量，根据此优化存储结构
+//集合本身就和hash，tree有关系，但是hash不讲团，不讲区间，但是树讲
+//集合不讲顺序，但是为了不讲顺序，需要先有散列概念或者顺序概念，又要有区间区段概念，就只能是树 堆？（优先队列）
 class IdxnoDisjointSetApp {
     public static void main(String[] args) {
         System.out.println("aa");
