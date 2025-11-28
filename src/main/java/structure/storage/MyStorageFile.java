724d2306fa891c0aacd42192f73e5ce56197b302
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-29 01:17:44 +0800

    draft

--- src/main/java/structure/storage/MyStorageFile.java
+++ src/main/java/structure/storage/MyStorageFile.java
@@ -1,5 +1,8 @@
 package structure.storage;
 
+//indexno 和 ref，是权衡，是动态，可以综合组合配合，本身存储线性，逻辑天然线性。其他逻辑结构都在这个线性存储结构模式下玩出花来
+//indexno 查块 写慢
+//ref 查慢 写块
 //InnerMem要管
 //OuterDisk也要管
 //适用的不同场景算法数据结构，例如树 胜者树 败者树（OuterDisk） 内外结合？排序时
