cc6fb4c3a57bb9d7815e3ce02ac50e1c3aff14d4
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-29 00:32:01 +0800

    draft

--- src/main/java/alg/idea/dependent/MyDependentFile.java
+++ src/main/java/alg/idea/dependent/MyDependentFile.java
@@ -10,7 +10,7 @@ package alg.idea.dependent;
 // 多算法 greedy dp 少数据结构本身（图优化 组合优化 线性优化） 状态 转移方程？代价？ 无数字运算
 //和系统设计 System Design hard 具体业务相关
 //时间复杂度 greedy O(n) dp O(n2)O(n3)O(2n) 准实时处理 或 离线处理
-//再往上hard 就要近似了 启发了
+//再往上hard 就要近似了 概率 随机 启发
 class MyDependentApp {
     public static void main(String[] args) {
         System.out.println("aa");
