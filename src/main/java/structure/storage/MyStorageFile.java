4f916879d164c180c8624defb2f8e80252ab1973
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-29 01:01:57 +0800

    draft

---
+++ src/main/java/structure/storage/MyStorageFile.java
@@ -0,0 +1,10 @@
+package structure.storage;
+
+//InnerMem要管
+//OuterDisk也要管
+//适用的不同场景算法数据结构，例如树 胜者树 败者树（OuterDisk） 内外结合？排序时
+class MyStorageApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
