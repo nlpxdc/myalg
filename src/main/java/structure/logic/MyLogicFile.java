a99a4b8023da77034d8bcc48675dd1341c408129
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-29 01:32:53 +0800

    draft

--- src/main/java/structure/logic/MyLogicFile.java
+++ src/main/java/structure/logic/MyLogicFile.java
@@ -4,6 +4,7 @@ package structure.logic;
 //indexno 3种 连续 hash 静态链表
 //ref 包含单向 双向 meta的元标记单个 多个等
 //同时给出每种实现下的时空复杂度，如果不全部写，至少思考下，思考全了，选最好的去写去实现
+//处理元素可以不只是单纯传统的数字可算术计算，还包括向量（一维数组，字符串） 以及广义表（二维数组，字符串数组，甚至更高）
 class MyLogicApp {
     public static void main(String[] args) {
         System.out.println("aa");
--- src/main/java/structure/storage/MyStorageFile.java
+++ src/main/java/structure/storage/MyStorageFile.java
@@ -6,6 +6,7 @@ package structure.storage;
 //InnerMem要管
 //OuterDisk也要管
 //适用的不同场景算法数据结构，例如树 胜者树 败者树（OuterDisk） 内外结合？排序时
+//局部缓存行原理 时间局部近 空间局部近 预取等，都是图灵机理论计算机模型，冯诺依曼体系结构，工程实现造成的局部性原理，深入理解计算机系统内容
 class MyStorageApp {
     public static void main(String[] args) {
         System.out.println("aa");
