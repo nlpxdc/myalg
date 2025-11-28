1262c54393268f0c42b716cf986b38ac0d9f0620
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-29 01:37:37 +0800

    draft

--- src/main/java/structure/logic/MyLogicFile.java
+++ src/main/java/structure/logic/MyLogicFile.java
@@ -5,6 +5,8 @@ package structure.logic;
 //ref 包含单向 双向 meta的元标记单个 多个等
 //同时给出每种实现下的时空复杂度，如果不全部写，至少思考下，思考全了，选最好的去写去实现
 //处理元素可以不只是单纯传统的数字可算术计算，还包括向量（一维数组，字符串） 以及广义表（二维数组，字符串数组，甚至更高）
+//线性 树 图 ，元素的邻接关系划分
+//map 元素的关联关系划分，所以独立
 class MyLogicApp {
     public static void main(String[] args) {
         System.out.println("aa");
