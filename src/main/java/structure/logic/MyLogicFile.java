d6f118b40a2319aa4625fb7459bc3cbc7b625d9c
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-29 01:23:46 +0800

    draft

---
+++ src/main/java/structure/logic/MyLogicFile.java
@@ -0,0 +1,11 @@
+package structure.logic;
+
+//每种逻辑结构，都要考虑用indexno 和 ref
+//indexno 3种 连续 hash 静态链表
+//ref 包含单向 双向 meta的元标记单个 多个等
+//同时给出每种实现下的时空复杂度，如果不全部写，至少思考下，思考全了，选最好的去写去实现
+class MyLogicApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
