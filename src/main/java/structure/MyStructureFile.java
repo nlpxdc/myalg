6a43b46d5ea809f0c87f2a1af6012ac4a1462e1a
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-11-30 22:58:09 +0800

    draft

---
+++ src/main/java/structure/MyStructureFile.java
@@ -0,0 +1,8 @@
+package structure;
+
+//本质上就是对结构的描述和控制。我有的是存储结构，我要的是逻辑结构，前者是已有条件，后者是目标要求。用已有条件实现目标要求
+class MyStructure {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
