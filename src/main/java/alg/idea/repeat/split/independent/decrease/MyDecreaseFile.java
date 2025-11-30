c6560d1cf2b5d049e189e707e592bbb21066450e
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-12-01 00:40:37 +0800

    draft

--- src/main/java/alg/idea/repeat/split/independent/decrease/MyDecreaseFile.java
+++ src/main/java/alg/idea/repeat/split/independent/decrease/MyDecreaseFile.java
@@ -1,6 +1,6 @@
 package alg.idea.repeat.split.independent.decrease;
 
-//拆解
+//拆解 也叫reduce
 public class MyDecreaseFile {
     public static void main(String[] args) {
         System.out.println("aa");
