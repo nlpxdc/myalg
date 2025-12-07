48d57ddff7b0616b8201df9cad9bcdf3816306f4
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-12-08 01:36:14 +0800

    draft

---
+++ src/main/java/structure/logic/ve/valcal/skiplist/MySkipListFile.java
@@ -0,0 +1,9 @@
+package structure.logic.ve.valcal.skiplist;
+
+//跳表最适合并发内存，所以jdk只有并发包有具体实现，并发友好
+//介于线性表和树bst之间，实现也简单
+class MySkipListApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
