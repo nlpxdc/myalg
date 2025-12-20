2dfabc7d17c1444703c7088d0fe49d0d84d255fe
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-12-20 20:02:38 +0800

    draft

--- src/main/java/leetcode/p01twosum/TwoSumFile.java
+++ src/main/java/leetcode/p01twosum/TwoSumFile.java
@@ -1,18 +1,20 @@
 package leetcode.p01twosum;
 
+import java.util.HashMap;
+
 class TwoSumApp {
     public static void main(String[] args) {
         System.out.println("aa");
     }
 
     public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if (i != j) {
-                    if (nums[i] + nums[j] == target) {
-                        return new int[]{i,j};
-                    }
-                }
+            int delta = target - nums[i];
+            if (map.get(delta) != null) {
+                return new int[]{map.get(delta), i};
+            } else {
+                map.put(nums[i], i);
             }
         }
         return null;
