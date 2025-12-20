85ba03e8e1f68e813fa22ff84a6bc8150dfcd525
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-12-20 19:41:12 +0800

    draft

--- src/main/java/leetcode/p01twosum/TwoSumFile.java
+++ src/main/java/leetcode/p01twosum/TwoSumFile.java
@@ -7,7 +7,7 @@ class TwoSumApp {
 
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
+            for (int j = i+1; j < nums.length; j++) {
                 if (i != j) {
                     if (nums[i] + nums[j] == target) {
                         return new int[]{i,j};
@@ -17,4 +17,30 @@ class TwoSumApp {
         }
         return null;
     }
+
+//    public static int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i+1; j < nums.length; j++) {
+//                if (i != j) {
+//                    if (nums[i] + nums[j] == target) {
+//                        return new int[]{i,j};
+//                    }
+//                }
+//            }
+//        }
+//        return null;
+//    }
+
+//    public static int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = 0; j < nums.length; j++) {
+//                if (i != j) {
+//                    if (nums[i] + nums[j] == target) {
+//                        return new int[]{i,j};
+//                    }
+//                }
+//            }
+//        }
+//        return null;
+//    }
 }
