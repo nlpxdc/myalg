0d3f22ecdd7f293853bed8a8f50c2c78646c3f4d
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-12-25 22:26:07 +0800

    draft

--- src/main/java/leetcode/p15threesum/ThreeSumFile.java
+++ src/main/java/leetcode/p15threesum/ThreeSumFile.java
@@ -5,80 +5,115 @@ import java.util.*;
 class ThreeSumApp {
     public static void main(String[] args) {
         System.out.println("aa");
-        int[] nums = {-1,0,1,2,-1,-4};
+        int[] nums = {-100,-70,-60,110,120,130,160};
         ThreeSumApp threeSumApp = new ThreeSumApp();
         List<List<Integer>> lists = threeSumApp.threeSum(nums);
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
-//        Arrays.sort(nums);
-//        if (nums[0] == 0 && nums[nums.length-1] == 0) {
-//            return Collections.singletonList(Arrays.asList(0, 0, 0));
-//        }
-        nums = Arrays.stream(nums).distinct().sorted().toArray();
+        ArrayList<List<Integer>> listList = new ArrayList<>();
 
+        HashSet<Long> set = new HashSet<>();
 
-        List<List<Integer>> listList = new ArrayList<>();
-        HashSet<String> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            int delta = -nums[i];
-            List<int[]> aryList = twoSum(nums, delta);
-            for (int[] ints : aryList) {
-                int x = nums[i], y = nums[ints[0]], z = nums[ints[1]];
-                if (x > y) { int t = x; x = y; y = t;}
-                if (x > z) { int t = x; x = z; z = t;}
-                if (y > z) { int t = y; y = z; z = t;}
-                String str = String.format("%d,%d,%d", x, y, z);
-                if (!set.contains(str)) {
-                    listList.add(Arrays.asList(nums[i], nums[ints[0]], nums[ints[1]]));
-                    set.add(str);
+        int zeroCnt = 0;
+        HashMap<Integer, Integer> positiveMap = new HashMap<>();
+        HashMap<Integer, Integer> negativeMap = new HashMap<>();
+
+        for (int num : nums) {
+            if (num == 0) {
+                if (zeroCnt < 3) {
+                    zeroCnt++;
+                }
+            } else if (num > 0) {
+                Integer positiveNumCnt = positiveMap.getOrDefault(num, 0);
+                if (positiveNumCnt < 2) {
+                    positiveMap.put(num, positiveNumCnt+1);
+                }
+            } else if (num < 0) {
+                Integer negativeNumCnt = negativeMap.getOrDefault(num, 0);
+                if (negativeNumCnt < 2) {
+                    negativeMap.put(num, negativeNumCnt+1);
                 }
             }
         }
+
+        //1. 000
+        if (zeroCnt ==3) {
+            listList.add(Arrays.asList(0,0,0));
+        }
+
+        //2. 2-1-1
+        for (Integer positiveNum : positiveMap.keySet()) {
+//            if (positiveNum % 2 == 0)
+            if ((positiveNum & 1) == 0) {
+//                int shouldNegativeNum = -positiveNum /2;
+                int shouldNegativeNum = (-positiveNum) >>1;
+                Integer shouldNegativeNumCnt = negativeMap.get(shouldNegativeNum);
+                if (shouldNegativeNumCnt != null && shouldNegativeNumCnt == 2) {
+                    listList.add(Arrays.asList(positiveNum, shouldNegativeNum, shouldNegativeNum));
+                }
+            }
+        }
+
+        //3. -211
+        for (Integer negativeNum : negativeMap.keySet()) {
+            if ((negativeNum & 1) == 0) {
+//                 int shouldPositiveNum = -negativeNum /2;
+                int shouldPositiveNum = (-negativeNum) >>1;
+                Integer shouldPositiveNumCnt = positiveMap.get(shouldPositiveNum);
+                if (shouldPositiveNumCnt != null && shouldPositiveNumCnt == 2) {
+                    listList.add(Arrays.asList(negativeNum, shouldPositiveNum, shouldPositiveNum));
+                }
+            }
+        }
+
+        //4. -101
+        if (zeroCnt >=1 ) {
+            for (Integer negativeNum : negativeMap.keySet()) {
+                if (positiveMap.containsKey(-negativeNum)) {
+                    listList.add(Arrays.asList(0, negativeNum, -negativeNum));
+                }
+            }
+        }
+
+        //5. -1-3 4
+        for (Integer positiveNum : positiveMap.keySet()) {
+            for (Integer negativeNum : negativeMap.keySet()) {
+                int delta = -positiveNum-negativeNum;
+                long key = key(negativeNum, delta);
+                if (delta != negativeNum && negativeMap.containsKey(delta) && !set.contains(key)) {
+                    listList.add(Arrays.asList(positiveNum, negativeNum, delta));
+                    set.add(key);
+                }
+            }
+        }
+
+        //6. -413
+        for (Integer negativeNum : negativeMap.keySet()) {
+            for (Integer positiveNum : positiveMap.keySet()) {
+                int delta = -negativeNum-positiveNum;
+                long key = key(positiveNum, delta);
+                if (delta != positiveNum && positiveMap.containsKey(delta) && !set.contains(key)) {
+                    listList.add(Arrays.asList(negativeNum, positiveNum, delta));
+                    set.add(key);
+                }
+            }
+        }
+
         return listList;
     }
 
-    private List<int[]> twoSum(int[] nums, int target) {
-        List<int[]> arrayList = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int delta = target - nums[i];
-            if (map.get(delta) != null) {
-                arrayList.add(new int[]{map.get(delta), i});
-            } else {
-                map.put(nums[i], i);
-            }
-        }
-        return arrayList;
+    private long key(int x, int y) {
+        if (x > y) {int t = x; x = y; y = t;}
+//        return ((long) x << 32) | ((long) y) ;
+        return ((long) x & 0xFFFFFFFFL) << 32 | (y & 0xFFFFFFFFL);
     }
 
-//    public static List<List<Integer>> threeSum(int[] nums) {
-//
-//        HashSet<Long> set = new HashSet<>();
-//        List<List<Integer>> listList = new ArrayList<>();
-//        for (int i = 0; i < nums.length; i++) {
-//            for (int j = i+1; j < nums.length; j++) {
-//                for (int k = j+1; k < nums.length; k++) {
-//                    if (i != j && i != k && j != k) {
-//                        if (nums[i]+nums[j]+nums[k] == 0) {
-//                            long key = key(nums[i], nums[j], nums[k]);
-//                            if (!set.contains(key)) {
-//                                listList.add(Arrays.asList(nums[i],nums[j],nums[k]));
-//                                set.add(key);
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        return listList;
+//    private long key(int x, int y, int z) {
+//        if (x > y) { int t = x; x = y; y = t;}
+//        if (x > z) { int t = x; x = z; z = t;}
+//        if (y > z) { int t = y; y = z; z = t;}
+//        return (((long) x) << 42) | (((long) y) << 21) | (((long) z)) ;
 //    }
-
-    private long key(int x, int y, int z) {
-        if (x > y) { int t = x; x = y; y = t;}
-        if (x > z) { int t = x; x = z; z = t;}
-        if (y > z) { int t = y; y = z; z = t;}
-        return (((long) x) << 42) | (((long) y) << 21) | (((long) z)) ;
-    }
 
 }
