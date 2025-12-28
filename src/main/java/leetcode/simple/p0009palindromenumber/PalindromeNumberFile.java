3f2c0f1891d4d59b46491fd67844c2ed5ed3b968
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-12-28 21:27:54 +0800

    draft

---
+++ src/main/java/leetcode/simple/p0009palindromenumber/PalindromeNumberFile.java
@@ -0,0 +1,24 @@
+package leetcode.simple.p0009palindromenumber;
+
+class PalindromeNumberApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+        PalindromeNumberApp app = new PalindromeNumberApp();
+        int x = 121;
+        boolean palindrome = app.isPalindrome(x);
+
+    }
+
+    public boolean isPalindrome(int x) {
+        Integer boxedX = new Integer(x);
+        String xStr = boxedX.toString();
+        char[] xCharAry = xStr.toCharArray();
+        for (int i = 0, j = xCharAry.length-1; i < xCharAry.length/2 && j >= xCharAry.length/2; i++, j--) {
+            if (xCharAry[i] != xCharAry[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
