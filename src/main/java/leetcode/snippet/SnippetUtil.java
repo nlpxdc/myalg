d1d30096139c9195bde16cc92f5920102d5a04a4
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-12-31 23:29:16 +0800

    draft

--- src/main/java/leetcode/snippet/SnippetUtil.java
+++ src/main/java/leetcode/snippet/SnippetUtil.java
@@ -3,6 +3,7 @@ package leetcode.snippet;
 public class SnippetUtil {
 
     public static void swap(int[] ary, int idxLeft, int idxRight) {
+        if (idxLeft == idxRight) return;
         swap0(ary,idxLeft, idxRight);
     }
 
@@ -13,9 +14,37 @@ public class SnippetUtil {
     }
 
     public static void swap1(int[] ary, int idxLeft, int idxRight) {
-        ary[idxLeft] = ary[idxLeft] + ary[idxRight];
+//        ary[idxLeft] = ary[idxLeft] + ary[idxRight];
+//        ary[idxRight] = ary[idxLeft] - ary[idxRight];
+//        ary[idxLeft] = ary[idxLeft] - ary[idxRight];
+
+        ary[idxLeft] += ary[idxRight];
         ary[idxRight] = ary[idxLeft] - ary[idxRight];
-        ary[idxLeft] = ary[idxLeft] - ary[idxRight];
+        ary[idxLeft] -= ary[idxRight];
+    }
+
+    public static void swap2(int[] ary, int idxLeft, int idxRight) {
+//        ary[idxLeft] = ary[idxLeft] * ary[idxRight];
+//        ary[idxRight] = ary[idxLeft] / ary[idxRight];
+//        ary[idxLeft] = ary[idxLeft] / ary[idxRight];
+
+        ary[idxLeft] *= ary[idxRight];
+        ary[idxRight] = ary[idxLeft] / ary[idxRight];
+        ary[idxLeft] /= ary[idxRight];
+    }
+
+    public static void swap3(int[] ary, int idxLeft, int idxRight) {
+//        ary[idxLeft] = ary[idxLeft] ^ ary[idxRight];
+//        ary[idxRight] = ary[idxLeft] ^ ary[idxRight];
+//        ary[idxLeft] = ary[idxLeft] ^ ary[idxRight];
+
+//        ary[idxLeft] ^= ary[idxRight];
+//        ary[idxRight] = ary[idxLeft] ^ ary[idxRight];
+//        ary[idxLeft] ^= ary[idxRight];
+
+        ary[idxLeft] ^= ary[idxRight];
+        ary[idxRight] ^= ary[idxLeft];
+        ary[idxLeft] ^= ary[idxRight];
     }
 
 }
