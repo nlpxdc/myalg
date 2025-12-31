21f97950765287e7cd21e2285ff8b0ab4958419f
陈捷飞 <chenjiefei@shizhuang-inc.com>
2025-12-31 22:42:59 +0800

    draft

---
+++ src/main/java/leetcode/snippet/MySnippetFile.java
@@ -0,0 +1,13 @@
+package leetcode.snippet;
+
+public class MySnippetFile {
+    public static void main(String[] args) {
+        System.out.println("hello cjf");
+        System.out.println("hello cjf");
+
+
+    }
+
+    
+
+}
