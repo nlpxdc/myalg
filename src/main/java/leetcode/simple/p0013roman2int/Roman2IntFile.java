90ef6a4ad342c0eb3f5c6e3e538c1a282067dcc3
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-01-02 18:41:21 +0800

    draft

--- src/main/java/leetcode/simple/p0013roman2int/Roman2IntFile.java
+++ src/main/java/leetcode/simple/p0013roman2int/Roman2IntFile.java
@@ -6,7 +6,7 @@ import java.util.Map;
 class Roman2IntApp {
     public static void main(String[] args) {
         System.out.println("aa");
-        int i = romanToInt("MMCCCXCIX");
+        int i = romanToInt("MCMXCIV");
     }
 
     public static int romanToInt(String s) {
@@ -24,31 +24,70 @@ class Roman2IntApp {
         //真实逻辑
         int ret = 0;
         char[] charArray = s.toCharArray();
-        for (int i = 0; i < charArray.length; i++) {
+        for (int i = charArray.length-1, preNum=0; i >=0; i--) {
             char c = charArray[i];
             Integer cNum = map.get(c);
-            ret += cNum;
-        }
-
-        if (charArray.length <=1) {
-            return ret;
-        }
-
-        for (int i = 0,j = 1; i < charArray.length-1 && j < charArray.length; i++,j++) {
-            char iChar = charArray[i];
-            char jChar = charArray[j];
-            if (iChar == 'I' && (jChar == 'V' || jChar == 'X')) {
-//                ret -= 1;
-//                ret -= 1;
-                ret -= 2;
-                i++;j++;
-            } else if (iChar == 'X' && (jChar == 'L' || jChar == 'C')) {
-                ret -= 20;
-                i++;j++;
-            } else if (iChar == 'C' && (jChar == 'D') || jChar == 'M') {
-                ret -= 200;
+            if (cNum < preNum) {
+                ret -= cNum;
+            } else {
+                ret += cNum;
             }
+            preNum=cNum;
         }
+
+//        for (int i = charArray.length-1; i >= 0; i--) {
+//            char c = charArray[i];
+//            if (c == 'V' || c =='X') {
+//                i--;
+//                if (i <0) {break;}
+//                if (charArray[i] == 'I') {
+//                    ret -= 2;
+//                }
+//                i--;
+//                if (i <0) {break;}
+//                if (charArray[i] == 'I') {
+//                    ret -= 2;
+//                }
+//            } else if (c == 'L' || c == 'C') {
+//                i--;
+//                if (i <0) {break;}
+//                if (charArray[i] == 'X') {
+//                    ret -= 20;
+//                }
+//                i--;
+//                if (i <0) {break;}
+//                if (charArray[i] == 'X') {
+//                    ret -= 20;
+//                }
+//            } else if (c == 'D' || c == 'M') {
+//                i--;
+//                if (i <0) {break;}
+//                if (charArray[i] == 'C') {
+//                    ret -= 200;
+//                }
+//                i--;
+//                if (i <0) {break;}
+//                if (charArray[i] == 'C') {
+//                    ret -= 200;
+//                }
+//            }
+//        }
+
+//        for (int i = 0,j = 1; i < charArray.length-1 && j < charArray.length; i++,j++) {
+//            char iChar = charArray[i];
+//            char jChar = charArray[j];
+//            if (iChar == 'I' && (jChar == 'V' || jChar == 'X')) {
+////                ret -= 1;
+////                ret -= 1;
+//                ret -= 2;
+//                i++;j++;
+//            } else if (iChar == 'X' && (jChar == 'L' || jChar == 'C')) {
+//                ret -= 20;
+//                i++;j++;
+//            } else if (iChar == 'C' && (jChar == 'D') || jChar == 'M') {
+//                ret -= 200;
+//            }
+//        }
 
         return ret;
     }
