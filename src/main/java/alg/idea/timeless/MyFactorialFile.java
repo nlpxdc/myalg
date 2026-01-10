54c7fa39c8f7e47f840aa138f81485ededb3eadb
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-01-10 20:43:18 +0800

    draft

---
+++ src/main/java/alg/idea/timeless/MyFactorialFile.java
@@ -0,0 +1,7 @@
+package alg.idea.timeless;
+
+class MyFactorialApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
--- src/main/java/operation/package-info.java
+++ src/main/java/operation/package-info.java
@@ -1,1 +1,5 @@
 package operation;
+
+//ologn可以人为约等于o1
+//主要用来处理t问题
+//含预处理
