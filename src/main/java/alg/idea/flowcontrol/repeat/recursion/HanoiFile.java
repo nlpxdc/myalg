fd06cdf7864e55c6c46fca1fb4fb52e41c79e75c
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-01-16 01:15:04 +0800

    draft

--- src/main/java/alg/idea/flowcontrol/repeat/recursion/HanoiFile.java
+++ src/main/java/alg/idea/flowcontrol/repeat/recursion/HanoiFile.java
@@ -1,9 +1,16 @@
 package alg.idea.flowcontrol.repeat.recursion;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 class HanoiApp {
     public static void main(String[] args) {
 //        System.out.println("aa");
         move(3);
+        System.out.println("---");
+        System.out.println("---");
+        move2(3);
     }
 
     private static void move(int n) {
@@ -21,6 +28,37 @@ class HanoiApp {
         System.out.println(String.format("moving disk%d , %s -> %s", n, nSrc, nDst));
         move(n-1, nAux, nSrc ,nDst);
 //        System.out.println(String.format("end move n%d , src:%s aux:%s dst:%s", n, nSrc, nAux, nDst));
+    }
+
+    private static void move2(int n) {
+        Deque<Move> moveStack = new LinkedList<>();
+        moveStack.push(new Move(n, 'A', 'B', 'C'));
+
+        for ( ; !moveStack.isEmpty() ; ) {
+            Move move = moveStack.pop();
+            if (move.n == 1) {
+                System.out.println(String.format("moving, %s -> %s", move.nSrc, move.nDst));
+            } else {
+                moveStack.push(new Move(move.n-1, move.nAux, move.nSrc ,move.nDst));
+                moveStack.push(new Move(1, move.nSrc, move.nAux , move.nDst));
+                moveStack.push(new Move(move.n-1, move.nSrc, move.nDst ,move.nAux));
+            }
+        }
+    }
+
+}
+
+class Move {
+    int n;
+    char nSrc;
+    char nAux;
+    char nDst;
+
+    Move(int n, char nSrc, char nAux, char nDst) {
+        this.n = n;
+        this.nSrc = nSrc;
+        this.nAux = nAux;
+        this.nDst = nDst;
     }
 
 }
