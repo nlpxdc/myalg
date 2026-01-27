8c75ebca501c011ebf7f11bac803049be58c5504
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-01-27 23:42:17 +0800

    draft

--- src/main/java/operation/deterministic/sort/linear/divide1/S1aBubbleSortFile.java
+++ src/main/java/operation/deterministic/sort/linear/divide1/S1aBubbleSortFile.java
@@ -1,4 +1,30 @@
 package operation.deterministic.sort.linear.divide1;
 
 public class S1aBubbleSortFile {
+    public static void main(String[] args) {
+
+    }
+
+    private static void bubbleSort(int[] ary) {
+        for (int i = 0; i < ary.length; i++) {
+            singleBubbleSort(ary);
+        }
+    }
+
+    private static void singleBubbleSort(int[] ary) {
+        for (int i = 0;
+             i < ary.length-1;
+             i++) {
+            if (ary[i] > ary[i+1]) {
+                swap(ary, i, i+1);
+            }
+        }
+    }
+
+    private static void swap(int[] ary, int idxA, int idxB) {
+        int t = ary[idxA];
+        ary[idxA] = ary[idxB];
+        ary[idxB] = t;
+    }
+
 }
