e6fef2466dfafb6c28ad738345c4edeb8e7f5f9a
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-01-27 23:57:23 +0800

    draft

--- src/main/java/operation/deterministic/sort/linear/divide1/S1aBubbleSortFile.java
+++ src/main/java/operation/deterministic/sort/linear/divide1/S1aBubbleSortFile.java
@@ -7,20 +7,28 @@ public class S1aBubbleSortFile {
 
     private static void bubbleSort(int[] ary) {
         for (int i = 0;
-             i < ary.length;
+             i < ary.length-1;
              i++) {
-            singleBubbleSort(ary, i);
+            boolean hasSwap = singleBubbleSort(ary, i);
+            //没有交换过，那么说明已经有序，跳出循环
+            if (!hasSwap) {
+//                break;
+                return;
+            }
         }
     }
 
-    private static void singleBubbleSort(int[] ary, int round) {
+    private static boolean singleBubbleSort(int[] ary, int round) {
+        boolean hasSwap = false;
         for (int i = 0;
              i < ary.length-1-round;
              i++) {
             if (ary[i] > ary[i+1]) {
                 swap(ary, i, i+1);
+                hasSwap = true;
             }
         }
+        return hasSwap;
     }
 
     private static void swap(int[] ary, int idxA, int idxB) {
