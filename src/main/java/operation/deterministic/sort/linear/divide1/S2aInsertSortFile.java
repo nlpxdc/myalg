21ab7c8b41bc858eae123c512dbe1ccc9d3b8d6c
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-01-29 23:41:15 +0800

    draft

--- src/main/java/operation/deterministic/sort/linear/divide1/S2aInsertSortFile.java
+++ src/main/java/operation/deterministic/sort/linear/divide1/S2aInsertSortFile.java
@@ -1,4 +1,31 @@
 package operation.deterministic.sort.linear.divide1;
 
-public class S2aInsertSortFile {
+class S2aInsertSortApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+        int[] ary = {9,8,7,6,5,4,3,2,1};
+        S2aInsertSortApp.insertSort(ary);
+    }
+
+    public static void insertSort(int[] ary) {
+        for (int i = 1; i < ary.length; i++) {
+            singleInsertSort(ary, i);
+        }
+    }
+
+    private static void singleInsertSort(int[] ary, int idx) {
+        int t = ary[idx];
+        int i = idx-1;
+        for (; i >= 0; i--) {
+            if (ary[i] > t) {
+                ary[i+1]=ary[i];
+            } else {
+                break;
+            }
+        }
+        if (i != idx-1) {
+            ary[i+1]=t;
+        }
+    }
+
 }
