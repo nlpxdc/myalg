607d75a972d808b3c52a7818c5740ff97917cb47
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-01-31 22:35:16 +0800

    draft

--- src/main/java/operation/deterministic/search/linear/decrease2/BinarySearchFile.java
+++ src/main/java/operation/deterministic/search/linear/decrease2/BinarySearchFile.java
@@ -8,5 +8,34 @@ class BinarySearchApp {
     }
 
 //    int searchIdx() {}
-
+    int binarySearchIdx(int[] ary, int val) {
+        if (val < ary[0]) {
+            return -1;
+        } else if (ary[ary.length-1] < val) {
+            return -1;
+        } else if (val == ary[0]) {
+            return 0;
+        } else if (val == ary[ary.length-1]) {
+            return ary.length-1;
+        } else {
+            for (int leftIdx = 0, rightIdx=ary.length-1
+                 ; leftIdx < ary.length && rightIdx >= 0 && leftIdx<=rightIdx;
+            ) {
+                //获取中间位置
+                int midIdx = (leftIdx+rightIdx)/2;
+                if (val < ary[midIdx]) {
+                    rightIdx = midIdx;
+                    continue;
+                } else if (ary[midIdx] < val) {
+                    leftIdx = midIdx;
+                    continue;
+                } else if (val == ary[midIdx]) {
+                    return midIdx;
+                } else {
+                    //noway
+                }
+            }
+        }
+        return -1;
+    }
 }
