30431f1f5429bb0c2ba904ee38466a12f07b98a6
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-01-31 12:46:36 +0800

    draft

--- src/main/java/operation/deterministic/search/linear/decrease1/LinearScanSearchFile.java
+++ src/main/java/operation/deterministic/search/linear/decrease1/LinearScanSearchFile.java
@@ -1,5 +1,37 @@
 package operation.deterministic.search.linear.decrease1;
 
 //未知序，无序，也可有序
-public class LinearScanSearchFile {
+class LinearScanSearchApp {
+    public static void main(String[] args) {
+
+    }
+
+    int searchIdx(int[] ary, int val) {
+        for (int i = 0; i < ary.length; i++) {
+            if (ary[i] == val) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    int searchIdx2(int[] ary, int val) {
+        if (ary == null || ary.length==0) {
+            return -1;
+        }
+        if (val < ary[0]) {
+            return 0;
+        }
+        if (val > ary[ary.length-1]) {
+            return ary.length;
+        }
+
+        for (int i = 0; i < ary.length-1; i++) {
+            if (ary[i] < val && val <= ary[i+1]) {
+                return i+1;
+            }
+        }
+        return ary.length;
+    }
+
 }
--- src/main/java/operation/deterministic/search/linear/decrease2/BinarySearchFile.java
+++ src/main/java/operation/deterministic/search/linear/decrease2/BinarySearchFile.java
@@ -2,5 +2,11 @@ package operation.deterministic.search.linear.decrease2;
 
 //必须有序，小到大，大到小，带区间
 //必须预处理
-public class BinarySearchFile {
+class BinarySearchApp {
+    public static void main(String[] args) {
+
+    }
+
+//    int searchIdx() {}
+
 }
