cc88d6bbf321fff186d3c9c6b5558de6b05d1a80
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-02-02 00:16:34 +0800

    draft

---
+++ src/main/java/operation/deterministic/sort/tree/heap/AryBinaryHeapFile.java
@@ -0,0 +1,38 @@
+package operation.deterministic.sort.tree.heap;
+
+class AryBinaryHeapApp {
+}
+
+class AryBinaryHeap {
+    int[] ary;
+    int size;
+
+    AryBinaryHeap() {
+        ary = new int[10000];
+        size = 0;
+    }
+
+    AryBinaryHeap(int[] origAry) {
+        this.ary = new int[10000];
+        this.size = 0;
+        //根据origAry，建堆
+    }
+
+    void addVal(int val) {
+
+    }
+
+    int getMax() {
+        return -1;
+    }
+
+    int pull() {
+        return -1;
+    }
+
+    //堆化一个数组（原地）
+    void heapify(int[] ary) {
+
+    }
+
+}
