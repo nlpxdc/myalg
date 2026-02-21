c1bd07d1dac29bc4c55a8d4a79e4aac19bfafc25
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-02-21 11:19:49 +0800

    draft

--- src/main/java/alg/repeat/recursion/dfs/GiveChangeFile.java
+++ src/main/java/alg/repeat/recursion/dfs/GiveChangeFile.java
@@ -1,9 +1,6 @@
 package alg.repeat.recursion.dfs;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 class GiveChangeApp {
@@ -21,7 +18,7 @@ class GiveChangeApp {
 //                .mapToInt(Integer::intValue)
 //                .toArray();
         List<Integer> path = new ArrayList<>();
-        backtrack(coins, sum, new ArrayList<>(), result);
+        backtrack(coins, sum, path, result);
         return result;
     }
 
@@ -45,3 +42,47 @@ class GiveChangeApp {
     }
 
 }
+
+class GiveChangeApp2 {
+    public static void main(String[] args) {
+        System.out.println("aa");
+        Set<List<Integer>> lists = startBackTrack(new int[]{1, 3, 4}, 6);
+    }
+
+    static Set<List<Integer>> startBackTrack(int[] coins, int sum) {
+        Set<List<Integer>> result = new HashSet<>();
+//        Arrays.sort(coins);
+//        int[] sortCoins = IntStream.of(coins)
+//                .boxed()
+//                .sorted(Collections.reverseOrder())
+//                .mapToInt(Integer::intValue)
+//                .toArray();
+        List<Integer> path = new ArrayList<>();
+        backtrack(coins, sum, path, result);
+        return result;
+    }
+
+    static void backtrack(int[] sortCoins, int remain, List<Integer> path, Set<List<Integer>> result) {
+        if (sortCoins == null || sortCoins.length == 0) {
+            return;
+        }
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            List<Integer> copyPath = new ArrayList<>(path);
+            Collections.sort(copyPath);
+            result.add(copyPath);
+            return;
+        } else {
+            for (int coin : sortCoins) {
+                path.add(coin);
+                int depRemain = remain - coin;
+                backtrack(sortCoins, depRemain, path, result);
+                path.remove(path.size()-1);
+            }
+        }
+    }
+
+}
+
+
