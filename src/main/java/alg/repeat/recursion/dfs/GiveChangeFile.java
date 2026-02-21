e244013a821d91b26fab4865b0678c622a7efabd
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-02-21 11:06:51 +0800

    draft

--- src/main/java/alg/repeat/recursion/dfs/GiveChangeFile.java
+++ src/main/java/alg/repeat/recursion/dfs/GiveChangeFile.java
@@ -9,22 +9,39 @@ import java.util.stream.IntStream;
 class GiveChangeApp {
     public static void main(String[] args) {
         System.out.println("aa");
+        List<List<Integer>> lists = startBackTrack(new int[]{1, 3, 4}, 6);
     }
 
-    List<List<Integer>> startBackTrack(int[] coins, int sum) {
-//        Arrays.sort(coins);
-        int[] sortCoins = IntStream.of(coins)
-                .boxed()
-                .sorted(Collections.reverseOrder())
-                .mapToInt(Integer::intValue)
-                .toArray();
+    static List<List<Integer>> startBackTrack(int[] coins, int sum) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(sortCoins, sum, result);
+//        Arrays.sort(coins);
+//        int[] sortCoins = IntStream.of(coins)
+//                .boxed()
+//                .sorted(Collections.reverseOrder())
+//                .mapToInt(Integer::intValue)
+//                .toArray();
+        List<Integer> path = new ArrayList<>();
+        backtrack(coins, sum, new ArrayList<>(), result);
         return result;
     }
 
-    void backtrack(int[] sortCoins, int remain, List<List<Integer>> result) {
-
+    static void backtrack(int[] sortCoins, int remain, List<Integer> path, List<List<Integer>> result) {
+        if (sortCoins == null || sortCoins.length == 0) {
+            return;
+        }
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            result.add(new ArrayList<>(path));
+            return;
+        } else {
+            for (int coin : sortCoins) {
+                path.add(coin);
+                int depRemain = remain - coin;
+                backtrack(sortCoins, depRemain, path, result);
+                path.remove(path.size()-1);
+            }
+        }
     }
 
 }
