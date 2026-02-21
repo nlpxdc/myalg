e1db0b3bb5644b36a7c5b749ff3ea256322d0700
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-02-21 14:34:44 +0800

    draft

--- src/main/java/alg/repeat/recursion/dfs/GiveChangeFile.java
+++ src/main/java/alg/repeat/recursion/dfs/GiveChangeFile.java
@@ -123,3 +123,50 @@ class GiveChangeApp3 {
     }
 
 }
+
+class GiveChangeApp4 {
+
+    static List<Integer> minCntList;
+
+    public static void main(String[] args) {
+        System.out.println("aa");
+        List<Integer> list = giveChange(new int[]{1, 3, 4}, 6);
+    }
+
+    static List<Integer> giveChange(int[] coins, int sum) {
+//        List<Integer> result = null;
+        Arrays.sort(coins);
+        minCntList = new ArrayList<>(Collections.nCopies(sum, 1));
+        List<Integer> path = new ArrayList<>();
+        backtrack(coins, 0, sum, path);
+        return minCntList;
+    }
+
+    static void backtrack(int[] sortCoins, int coinStart, int remain, List<Integer> path) {
+        if (sortCoins == null || sortCoins.length == 0) {
+            return;
+        }
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            if (minCntList == null) {
+                minCntList = new ArrayList<>(path);
+            } else {
+                minCntList = path.size() < minCntList.size() ? new ArrayList<>(path) : minCntList;
+            }
+            return;
+        } else {
+            if (sortCoins[coinStart] > remain) {
+                return;
+            }
+            for (int i = coinStart; i < sortCoins.length; i++) {
+                int coin = sortCoins[i];
+                path.add(coin);
+                int depRemain = remain - coin;
+                backtrack(sortCoins, i, depRemain, path);
+                path.remove(path.size()-1);
+            }
+        }
+    }
+
+}
