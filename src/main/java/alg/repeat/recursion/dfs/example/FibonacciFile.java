41245a2e81066df8a650d58d56be331fff83ce52
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-02-22 12:27:21 +0800

    draft

--- src/main/java/alg/repeat/recursion/dfs/example/FibonacciFile.java
+++ src/main/java/alg/repeat/recursion/dfs/example/FibonacciFile.java
@@ -20,3 +20,39 @@ class FibonacciApp {
     }
 
 }
+
+class FibonacciApp2 {
+    public static void main(String[] args) {
+        System.out.println("aa");
+        for (int i = 0; i < 100; i++) {
+            long val = fibonacci(i);
+            if (val < 0) {
+                System.out.println("end break");
+            }
+            System.out.println(String.format("%d->%d", i, val));
+        }
+    }
+
+    static long fibonacci(long n) {
+        long[] dp = new long[10000];
+        return dfs(n, dp);
+    }
+
+    static long dfs(long n, long[] dp) {
+        long cache = dp[(int) n];
+        if (cache > 0) {
+            return cache;
+        }
+        if (n == 0) {
+            dp[0] = 0;
+            return 0;
+        } else if (n == 1) {
+            dp[1] = 1;
+            return 1;
+        } else if (n >= 2) {
+            return dfs(n-2, dp) + dfs(n-1, dp);
+        }
+        return -1;
+    }
+
+}
