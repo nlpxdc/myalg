b1d238ab570da6c95c42ae6f18027c5e7d2fd1c7
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-02-22 15:25:34 +0800

    draft

--- src/main/java/alg/repeat/recursion/dfs/example/FactorialFile.java
+++ src/main/java/alg/repeat/recursion/dfs/example/FactorialFile.java
@@ -26,3 +26,33 @@ class FactorialApp {
     }
 
 }
+
+class FactorialApp2 {
+    public static void main(String[] args) {
+        System.out.println("aa");
+        for (int i = 1; i < Integer.MAX_VALUE; i++) {
+            long val = factorial(i);
+            if (val < 0) {
+                System.out.println("end break");
+                break;
+            }
+            System.out.println(String.format("%d->%d", i, val));
+        }
+    }
+
+    static long factorial(int n) {
+        long[] dp = new long[100000];
+        return dfs(n, dp);
+    }
+
+    static long dfs(int n, long[] dp) {
+        if (n < 1) throw new RuntimeException();
+        long cache = dp[n];
+        if (cache > 0) return cache;
+        if (n == 1) return 1;
+//        if (n >= 2) return n * dfs(n-1);
+        if (n >= 2) return n + dfs(n-1, dp);
+        return -1;
+    }
+
+}
