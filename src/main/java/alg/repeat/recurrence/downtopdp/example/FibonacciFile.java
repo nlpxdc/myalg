653b92c9f2f86ae7512714b1b5bbd38f15cf83f5
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-02-24 20:41:10 +0800

    draft

--- src/main/java/alg/repeat/recurrence/downtopdp/example/FibonacciFile.java
+++ src/main/java/alg/repeat/recurrence/downtopdp/example/FibonacciFile.java
@@ -3,7 +3,7 @@ package alg.repeat.recurrence.downtopdp.example;
 class FibonacciApp {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            long val = fibonacci(i);
+            long val = fibonacci2(i);
             if (val < 0) {
                 System.out.println("end break");
                 break;
@@ -25,6 +25,25 @@ class FibonacciApp {
                 prev2Tab = t;
             }
             return prev2Tab + prev1Tab;
+        }
+
+        return -1;
+    }
+
+    static long fibonacci2(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n >= 2) {
+            long[] dp = new long[n+1];
+            dp[0] = 0;
+            dp[1] = 1;
+
+            for (int i = 2; i <= n; i++) {
+                dp[i] = dp[i-2] + dp[i-1];
+            }
+            return dp[n];
         }
 
         return -1;
