a614041f904c8d5fb7217c1a55b85c9a388c5517
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-02-27 21:32:57 +0800

    draft

--- src/main/java/alg/repeat/recurrence/bfs/example/SlidingWindowMaxFile.java
+++ src/main/java/alg/repeat/recurrence/bfs/example/SlidingWindowMaxFile.java
@@ -1,5 +1,10 @@
 package alg.repeat.recurrence.bfs.example;
 
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.PriorityQueue;
+
 class SlidingWindowMaxApp {
     public static void main(String[] args) {
         System.out.println("aa");
@@ -13,6 +18,50 @@ class SlidingWindowMaxApp {
             //取出区间内的最大值
             int iMax = windowMax(nums, i, k);
             ret[i] = iMax;
+        }
+
+        return ret;
+    }
+
+    int windowMax(int[] nums, int beginIdx, int k) {
+        int endIdx = beginIdx + k - 1;
+        int max = nums[beginIdx];
+        for (int i = beginIdx+1; i <= endIdx; i++) {
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+
+}
+
+class SlidingWindowMaxApp2 {
+    public static void main(String[] args) {
+        SlidingWindowMaxApp2 app = new SlidingWindowMaxApp2();
+        int[] ints = {1, 3, -1, -3, 5, 3, 6, 7};
+        int[] ints1 = app.maxSlidingWindow(ints, 3);
+    }
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        final int retLen = nums.length-k+1;
+        int[] ret = new int[retLen];
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+
+        //初始化priorityQueue
+        for (int i = 0; i < k; i++) {
+            priorityQueue.add(nums[i]);
+        }
+
+        for (int i = 0; i < retLen; i++) {
+            int max = priorityQueue.peek();
+            ret[i] = max;
+            int leftVal = nums[i];
+            priorityQueue.remove(leftVal);
+            if (i < retLen-1) {
+                int rightVal = nums[k+i];
+                priorityQueue.add(rightVal);
+            }
+
         }
 
         return ret;
