ae7fe28117c88b10a7fad2e3249db11cd249944c
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-03-02 23:01:42 +0800

    draft

--- src/main/java/alg/repeat/recurrence/dfs/example/LongestPalindromeSubstringFile.java
+++ src/main/java/alg/repeat/recurrence/dfs/example/LongestPalindromeSubstringFile.java
@@ -62,7 +62,7 @@ class LongestPalindromeSubstringApp2 {
         //中心点循环枚举
         int max = 0;
         int[] maxObj = new int[]{max, 0, 0};
-        for (int midIdx = 1; midIdx < preStr.length()-1; midIdx++) {
+        for (int midIdx = 0; midIdx < preStr.length(); midIdx++) {
             int[] midMaxObj = bePalindrome(preStr, midIdx);
             if (midMaxObj[0] > max) {
                 max = midMaxObj[0];
@@ -83,22 +83,22 @@ class LongestPalindromeSubstringApp2 {
     String preHandleStr(String s) {
 
         char[] charAry = new char[s.length() + s.length() + 1];
-        charAry[0] = '^';
+        charAry[0] = '#';
         for (int i = 0; i < s.length(); i++) {
             charAry[2*i+1] = s.charAt(i);
         }
         for (int i = 0; i < s.length()-1; i++) {
             charAry[2*i+2] = '#';
         }
-        charAry[charAry.length-1] = '$';
+        charAry[charAry.length-1] = '#';
         return new String(charAry);
     }
 
     int[] bePalindrome(String s, int midIdx) {
-        int max = 1;
+        int max = 0;
         int[] maxObj = new int[]{max, midIdx, midIdx};
-        for (int leftIdx = midIdx-1, rightIdx = midIdx+1;
-             leftIdx >= 0 && rightIdx <= s.length()-1 && s.charAt(leftIdx) != '^' && s.charAt(rightIdx) != '$' ;
+        for (int leftIdx = midIdx, rightIdx = midIdx;
+             leftIdx >= 0 && rightIdx <= s.length()-1;
              leftIdx--,rightIdx++) {
             char leftVal = s.charAt(leftIdx);
             char rightVal = s.charAt(rightIdx);
