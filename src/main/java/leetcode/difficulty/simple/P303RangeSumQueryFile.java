fa5e32e8c04118b495d643c7f5cdc934cec2382a
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-03-06 20:59:32 +0800

    draft

--- src/main/java/leetcode/difficulty/simple/P303RangeSumQueryFile.java
+++ src/main/java/leetcode/difficulty/simple/P303RangeSumQueryFile.java
@@ -22,3 +22,21 @@ class NumArray {
         return sum;
     }
 }
+
+class NumArray2 {
+
+    int[] prefixSums;
+
+    public NumArray2(int[] nums) {
+        prefixSums = new int[nums.length];
+        prefixSums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            prefixSums[i] += prefixSums[i-1] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        if (left==0) return prefixSums[right];
+        return prefixSums[right] - prefixSums[left - 1];
+    }
+}
