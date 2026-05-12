066d684e7957aabf95e96d3f35aab8eca7c3c53f
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-05-12 11:51:59 +0800

    draft

--- src/main/java/on/CoinFile.java
+++ src/main/java/on/CoinFile.java
@@ -1,7 +1,35 @@
 package on;
 
+import java.util.*;
+
 class CoinApp {
     public static void main(String[] args) {
         System.out.println("aa");
+        int coin = coin(46);
     }
+
+    static int[] dimos = {5,1,8,30,10};
+
+    static int coin(int target) {
+        List<List<Integer>> lists = coin0(target);
+        List<Integer> list = lists.stream().min(Comparator.comparingInt(List::size)).orElse(Collections.emptyList());
+
+        int ret = lists.stream().mapToInt(List::size).min().orElse(0);
+
+        return list.size(); //ret
+    }
+
+    static List<List<Integer>> coin0(int target) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        ArrayList<Integer> choseCoins = new ArrayList<>();
+        coinDfs(choseCoins, target, result);
+
+        return result;
+    }
+
+    static void coinDfs(List<Integer> chosenCoins, int remain, List<List<Integer>> result) {
+
+    }
+
 }
