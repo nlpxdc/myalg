3448578947f0b15f766edfb71de66508423af3f7
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-05-13 13:35:39 +0800

    draft

--- src/main/java/on/CoinFile2.java
+++ src/main/java/on/CoinFile2.java
@@ -6,9 +6,15 @@ import java.util.stream.Collectors;
 class CoinApp2 {
     public static void main(String[] args) {
         System.out.println("aa");
+        CoinApp2 app = new CoinApp2();
+//        int[] coins = {1,2,5};
+        int i = app.coinChange(new int[]{2}, 3);
     }
 
     public int coinChange(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
         //降序排列
         int[] descDenos = Arrays.stream(coins)
                 .boxed()
@@ -27,7 +33,7 @@ class CoinApp2 {
         int minSize = retList.stream()
                 .mapToInt(List::size)
                 .min()
-                .orElse(0);
+                .orElse(-1);
 //        List<List<Integer>> minList = retList.stream().filter(t -> t.size() == minSize).collect(Collectors.toList());
 
         return minSize;
@@ -38,25 +44,24 @@ class CoinApp2 {
     //remain 临时参数，输入
     //retList 返回数据，克隆类型，来源choseCoins
     void coinDfs(int[] coins, List<Integer> choseCoins, int remain, List<List<Integer>> retList) {
-        if (remain <= 0) {
-            return;
-        }
         //当前状态choseCoins，计算下一值，组成下一状态
         //可选值 remain减去choseCoins的
-        int currSum = choseCoins.stream().mapToInt(t -> t).sum();
-        int vault = remain - currSum;
         //正好匹配，结果值深拷贝后放入结果容器中
-        if (vault == 0) {
+        if (remain == 0) {
             retList.add(new ArrayList<>(choseCoins));
-        } else if (vault < 0) {
+            return;
+        } else if (remain < 0) {
             return;
         }
         //可以选中过滤出很多个可能
         //降序拿第一个就好
         int nextChoseCoin = Arrays.stream(coins)
-                .filter(t -> t <= vault)
+                .filter(t -> t <= remain)
                 .findFirst()
                 .orElse(0);
+        if (nextChoseCoin == 0) {
+            return;
+        }
         choseCoins.add(nextChoseCoin);
         coinDfs(coins, choseCoins, remain-nextChoseCoin, retList);
         choseCoins.remove(choseCoins.size()-1);
