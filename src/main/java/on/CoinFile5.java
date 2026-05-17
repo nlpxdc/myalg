182ae2393c20fc98a89f6c272b208b53144c65dc
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-05-17 23:02:54 +0800

    draft

--- src/main/java/on/CoinFile5.java
+++ src/main/java/on/CoinFile5.java
@@ -3,39 +3,45 @@ package on;
 import java.util.HashMap;
 import java.util.Map;
 
-class CoinApp4 {
+class CoinApp5 {
     public static void main(String[] args) {
         System.out.println("aa");
-        CoinApp4 app = new CoinApp4();
+        CoinApp5 app = new CoinApp5();
         int i = app.coinChange(new int[]{1, 2, 5}, 100);
     }
 
     public int coinChange(int[] coins, int amount) {
-        ThreadLocal<Integer> minSizeRef = new ThreadLocal<>();
-        coinDfs(amount, 0, amount, 0, coins, minSizeRef);
-        return minSizeRef.get() == null ? -1 : minSizeRef.get();
+        HashMap<Integer, Integer> dp = new HashMap<>();
+        coinDfs(amount, 0, amount, 0, coins, dp);
+        return dp.get(amount) == null ? -1 : dp.get(amount);
     }
 
     void coinDfs(int parentRemain, int parentCoin,
                  int remain, int selectedSize, int[] coins,
-                 ThreadLocal<Integer> minSizeRef) {
+                 Map<Integer, Integer> dp) {
         System.out.println("enter fun"+remain);
+
         if (remain == 0) {
-            Integer minSize = minSizeRef.get();
+            Integer minSize = dp.get(remain);
             if (minSize == null) {
-                minSizeRef.set(selectedSize);
+                dp.put(remain, selectedSize);
                 return;
             } else {
                 int retMinSize = Math.min(selectedSize, minSize);
-                minSizeRef.set(retMinSize);
+                dp.put(remain, retMinSize);
                 return;
             }
         } else if (remain < 0) {
+            dp.put(remain, -1);
             return;
         } else {
+            Integer minSize = dp.get(remain);
+            if (minSize != null) {
+                return;
+            }
             for (int i = 0; i < coins.length; i++) {
                 int coin = coins[i];
-                coinDfs(remain, coin, remain-coin, selectedSize+1, coins, minSizeRef);
+                coinDfs(remain, coin, remain-coin, selectedSize+1, coins, dp);
             }
         }
     }
