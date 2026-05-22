3816c64457bc5fdbadee7a7f5e279893873aa9b9
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-05-22 14:50:39 +0800

    draft

---
+++ src/main/java/on/KnapsackFile.java
@@ -0,0 +1,22 @@
+package on;
+
+class KnapsackApp {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 计算01背包问题的结果
+     * @param V int整型 背包的体积
+     * @param n int整型 物品的个数
+     * @param vw int整型二维数组 第一维度为n,第二维度为2的二维数组,vw[i][0],vw[i][1]分别描述i+1个物品的vi,wi
+     * @return int整型
+     */
+    public int knapsack (int V, int n, int[][] vw) {
+        // write code here
+        return -1;
+    }
+
+}
