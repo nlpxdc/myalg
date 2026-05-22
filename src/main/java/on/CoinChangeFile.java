77f1f5f21e112ac48d2b5e1915163bcf90796e9f
陈捷飞 <chenjiefei@shizhuang-inc.com>
2026-05-22 10:05:02 +0800

    draft

--- src/main/java/on/CoinChangeFile.java
+++ src/main/java/on/CoinChangeFile.java
@@ -77,12 +77,39 @@ class CoinChangeApp3 {
         Arrays.fill(dp, amount+1);
         dp[0] = 0;
 
+        //排列
         for (int i = 1; i <= amount; i++) {
             for (int j = 0; j < coins.length; j++) {
                 int coin = coins[j];
                 if (coin <= i) {
                     dp[i] = Math.min(dp[i], dp[i-coin]+1);
                 }
+            }
+        }
+
+        return dp[amount] == amount+1 ? -1 : dp[amount];
+    }
+
+}
+
+class CoinChangeApp4 {
+    public static void main(String[] args) {
+
+        System.out.println("aa");
+        CoinChangeApp4 app = new CoinChangeApp4();
+        int minSize = app.coinChange(new int[]{1, 2, 5}, 11);
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+
+        //组合
+        for (int i = 0; i < coins.length; i++) {
+            int coin = coins[i];
+            for (int j = coin; j <= amount; j++) {
+                dp[j] = Math.min(dp[j], dp[j-coin]+1);
             }
         }
 
